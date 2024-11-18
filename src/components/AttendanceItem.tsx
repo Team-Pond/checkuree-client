fde05e128a8b3517252885c0493e81b03fb2fafc
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-18 19:46:42 +0900

    Fix: 주석처리 해제

--- src/components/AttendanceItem.tsx
+++ src/components/AttendanceItem.tsx
@@ -1,103 +1,86 @@
-// 'use client';
+// Next
 
-// import React from 'react';
+// Styles
 
-// // Next
-// import Image from 'next/image';
+// Components
 
-// // Styles
-// import { Images } from '@/styles/globalStyles';
-// import {
-//     StatusButton,
-//     AttendanceItemContainer,
-// } from '@/styles/app/attendancesId.styles';
+// Types
 
-// // Components
-// import DetailInputBox from '@/app/attendances/_components/DetailInputBox';
+import { Images } from "../styles/globalStyles";
+import {
+  AttendanceItemContainer,
+  StatusButton,
+} from "../styles/app/attendancesId.styles";
+import { AttendanceSchedulesByDateItem } from "../api/schema";
+import DetailInputBox, { HandleListItemType } from "./DetailInputBox";
 
-// // Types
-// import { AttendanceSchedulesByDateItem } from '@/api/attendances/schema';
-// import { HandleListItemType } from '@/app/attendances/[id]/page';
+interface PropsType {
+  index: number;
+  time: string;
+  item: AttendanceSchedulesByDateItem;
+  handleListItem: HandleListItemType;
+}
 
-// interface PropsType {
-//     index: number;
-//     time: string;
-//     item: AttendanceSchedulesByDateItem;
-//     handleListItem: HandleListItemType;
-// }
+const AttendanceItem = (props: PropsType) => {
+  const { item, time, index, handleListItem } = props;
 
-// const AttendanceItem = (props: PropsType) => {
-//     const { item, time, index, handleListItem } = props;
+  const statusButtons: { label: string; value: string }[] = [
+    { label: "출석", value: "Present" },
+    { label: "지각", value: "Late" },
+    { label: "결석", value: "Absent" },
+  ];
 
-//     const statusButtons: { label: string; value: string }[] = [
-//         { label: '출석', value: 'Present' },
-//         { label: '지각', value: 'Late' },
-//         { label: '결석', value: 'Absent' },
-//     ];
+  const status =
+    item.newStatus && item.newStatus.length > 0 ? item.newStatus : item.status;
 
-//     const status =
-//         item.newStatus && item.newStatus.length > 0
-//             ? item.newStatus
-//             : item.status;
+  return (
+    <AttendanceItemContainer
+      status={status || ""}
+      isDetailOpen={item.isDetailOpen || false}
+      key={`attendance-item__${item.id}`}
+    >
+      <div className={"attendance-item__container"}>
+        <div className="name">
+          {item.attendee.name}
+          <img
+            className={item.isDetailOpen ? "detail-open" : ""}
+            src={Images.DetailOpen}
+            alt={`open-detail__${item.attendee.name}`}
+            onClick={() =>
+              handleListItem(index, time, "isDetailOpen", !item.isDetailOpen)
+            }
+          />
+        </div>
 
-//     return (
-//         <AttendanceItemContainer
-//             status={status || ''}
-//             isDetailOpen={item.isDetailOpen || false}
-//             key={`attendance-item__${item.id}`}
-//         >
-//             <div className={'attendance-item__container'}>
-//                 <div className="name">
-//                     {item.attendee.name}
-//                     <Image
-//                         className={item.isDetailOpen ? 'detail-open' : ''}
-//                         src={Images.DetailOpen}
-//                         alt={`open-detail__${item.attendee.name}`}
-//                         onClick={() =>
-//                             handleListItem(
-//                                 index,
-//                                 time,
-//                                 'isDetailOpen',
-//                                 !item.isDetailOpen
-//                             )
-//                         }
-//                     />
-//                 </div>
+        <div className="status-buttons">
+          {statusButtons.map((button) => (
+            <StatusButton
+              isSelected={status === button.value}
+              onClick={() => {
+                handleListItem(index, time, "newStatus", button.value);
+                // handleListItem(index, time, 'status', '');
 
-//                 <div className="status-buttons">
-//                     {statusButtons.map((button) => (
-//                         <StatusButton
-//                             isSelected={status === button.value}
-//                             onClick={() => {
-//                                 handleListItem(
-//                                     index,
-//                                     time,
-//                                     'newStatus',
-//                                     button.value
-//                                 );
-//                                 // handleListItem(index, time, 'status', '');
+                // if (button.value === 'Present') {
+                //     handleListItem(index, time, 'etc', '');
+                // }
+              }}
+              key={`status-button__${button.label}`}
+            >
+              {button.label}
+            </StatusButton>
+          ))}
+        </div>
+      </div>
 
-//                                 // if (button.value === 'Present') {
-//                                 //     handleListItem(index, time, 'etc', '');
-//                                 // }
-//                             }}
-//                             key={`status-button__${button.label}`}
-//                         >
-//                             {button.label}
-//                         </StatusButton>
-//                     ))}
-//                 </div>
-//             </div>
+      {/* 출석/지각/상세 사유 입력 박스 */}
+      <DetailInputBox
+        item={item}
+        time={time}
+        index={index}
+        handleListItem={handleListItem}
+      />
+    </AttendanceItemContainer>
+  );
+};
 
-//             {/* 출석/지각/상세 사유 입력 박스 */}
-//             <DetailInputBox
-//                 item={item}
-//                 time={time}
-//                 index={index}
-//                 handleListItem={handleListItem}
-//             />
-//         </AttendanceItemContainer>
-//     );
-// };
-
-// export default AttendanceItem;
+export default AttendanceItem;
--- src/components/DetailInputBox.tsx
+++ src/components/DetailInputBox.tsx
@@ -1,127 +1,99 @@
-// 'use client';
+"use client";
+// Components
+import { TextField } from "@mui/material";
 
-// import React from 'react';
+// Styles
 
-// // Components
-// import { TextField } from '@mui/material';
+import { DetailButton } from "../styles/app/attendancesId.styles";
+import { AttendanceSchedulesByDateItem } from "../api/schema";
 
-// // Styles
-// import { DetailButton } from '@/styles/app/attendancesId.styles';
+export type HandleListItemType = (
+  index: number,
+  time: string,
+  field: string,
+  value: string | boolean
+) => void;
 
-// // Types
-// import {
-//     AttendanceSchedulesByDateItem,
-//     Record,
-// } from '@/api/attendances/schema';
-// import { HandleListItemType } from '@/app/attendances/[id]/page';
+interface ItemType extends AttendanceSchedulesByDateItem {
+  lateTime?: string;
+  etc?: string;
+  absenceType?: string;
+}
 
-// interface ItemType extends AttendanceSchedulesByDateItem {
-//     lateTime?: string;
-//     etc?: string;
-//     absenceType?: string;
-// }
+interface PropsType {
+  index: number;
+  time: string;
+  item: ItemType;
+  handleListItem: HandleListItemType;
+}
 
-// interface PropsType {
-//     index: number;
-//     time: string;
-//     item: ItemType;
-//     handleListItem: HandleListItemType;
-// }
+const DetailInputBox = ({ item, time, index, handleListItem }: PropsType) => {
+  const detailOptions = {
+    Late: [
+      { label: "5분", value: "5m" },
+      { label: "10분", value: "10m" },
+      { label: "15분", value: "15m" },
+      { label: "20분 이상", value: "20m" },
+    ],
+    Absent: [
+      { label: "공결", value: "OFFICIAL" },
+      { label: "병결", value: "SICK" },
+      { label: "무단", value: "GENERAL" },
+      { label: "기타", value: "ETC" },
+    ],
+  };
 
-// const DetailInputBox = ({ item, time, index, handleListItem }: PropsType) => {
-//     const detailOptions = {
-//         Late: [
-//             { label: '5분', value: '5m' },
-//             { label: '10분', value: '10m' },
-//             { label: '15분', value: '15m' },
-//             { label: '20분 이상', value: '20m' },
-//         ],
-//         Absent: [
-//             { label: '공결', value: 'OFFICIAL' },
-//             { label: '병결', value: 'SICK' },
-//             { label: '무단', value: 'GENERAL' },
-//             { label: '기타', value: 'ETC' },
-//         ],
-//     };
+  const status =
+    item.newStatus && item.newStatus.length > 0 ? item.newStatus : item.status;
 
-//     const status =
-//         item.newStatus && item.newStatus.length > 0
-//             ? item.newStatus
-//             : item.status;
+  return (
+    <div className="detail-box">
+      {status === "Late" ? (
+        <div className="detail-buttons">
+          {detailOptions.Late.map((option) => (
+            <DetailButton
+              isSelected={option.value === item.lateTime}
+              onClick={() => {
+                handleListItem(index, time, "lateTime", option.value);
+                handleListItem(index, time, "newStatus", "Late");
+              }}
+              key={option.value}
+            >
+              {option.label}
+            </DetailButton>
+          ))}
+        </div>
+      ) : status === "Absent" ? (
+        <div className="detail-buttons">
+          {detailOptions.Absent.map((option) => (
+            <DetailButton
+              isSelected={option.value === item.absenceType}
+              onClick={() => {
+                handleListItem(index, time, "absenceType", option.value);
+                handleListItem(index, time, "newStatus", "Absent");
+              }}
+              key={option.value}
+            >
+              {option.label}
+            </DetailButton>
+          ))}
+        </div>
+      ) : null}
 
-//     return (
-//         <div className="detail-box">
-//             {status === 'Late' ? (
-//                 <div className="detail-buttons">
-//                     {detailOptions.Late.map((option) => (
-//                         <DetailButton
-//                             isSelected={option.value === item.lateTime}
-//                             onClick={() => {
-//                                 handleListItem(
-//                                     index,
-//                                     time,
-//                                     'lateTime',
-//                                     option.value
-//                                 );
-//                                 handleListItem(
-//                                     index,
-//                                     time,
-//                                     'newStatus',
-//                                     'Late'
-//                                 );
-//                             }}
-//                             key={option.value}
-//                         >
-//                             {option.label}
-//                         </DetailButton>
-//                     ))}
-//                 </div>
-//             ) : status === 'Absent' ? (
-//                 <div className="detail-buttons">
-//                     {detailOptions.Absent.map((option) => (
-//                         <DetailButton
-//                             isSelected={option.value === item.absenceType}
-//                             onClick={() => {
-//                                 handleListItem(
-//                                     index,
-//                                     time,
-//                                     'absenceType',
-//                                     option.value
-//                                 );
-//                                 handleListItem(
-//                                     index,
-//                                     time,
-//                                     'newStatus',
-//                                     'Absent'
-//                                 );
-//                             }}
-//                             key={option.value}
-//                         >
-//                             {option.label}
-//                         </DetailButton>
-//                     ))}
-//                 </div>
-//             ) : null}
+      <TextField
+        value={item.etc}
+        rows={status === "Present" ? 4 : 3}
+        onChange={(e) => {
+          handleListItem(index, time, "etc", e.target.value);
 
-//             <TextField
-//                 value={item.etc}
-//                 rows={status === 'Present' ? 4 : 3}
-//                 onChange={(e) => {
-//                     handleListItem(index, time, 'etc', e.target.value);
+          if (item.newStatus === "") {
+            handleListItem(index, time, "newStatus", item.status || "");
+          }
+        }}
+        multiline
+      />
+    </div>
+  );
+};
 
-//                     if (item.newStatus === '') {
-//                         handleListItem(
-//                             index,
-//                             time,
-//                             'newStatus',
-//                             item.status || ''
-//                         );
-//                     }
-//                 }}
-//                 multiline
-//             />
-//         </div>
-//     );
-// };
-
-// export default DetailInputBox;
+export default DetailInputBox;
