1ad171926cfe726dce025c30512b85381611e158
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-19 18:18:36 +0900

    Fix: BottomDrawer width 및 정렬 수정

--- src/components/BottomDrawer/index.tsx
+++ src/components/BottomDrawer/index.tsx
@@ -17,9 +17,11 @@ const BottomDrawer = ({ open, onClose, children }: PropsType) => {
       <Global
         styles={css`
           .MuiPaper-root {
+            width: 375px;
             height: 95vh !important;
             border-top-left-radius: 32px;
             border-top-right-radius: 32px;
+            margin: 0 auto; /* 가로 가운데 정렬 */
           }
         `}
       />
