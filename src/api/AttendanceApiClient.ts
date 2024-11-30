41015dca1b0158f51cf6643e776169442fe3d4f8
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-30 14:56:16 +0900

    Update: AuthAPi 삭제

--- src/api/AttendanceApiClient.ts
+++ src/api/AttendanceApiClient.ts
@@ -5,7 +5,6 @@ import {
   CreateRecordsRequest,
   CreateSchedulesRequest,
   DeleteAttendeesRequest,
-  LoginDataRequest,
   UseApiProps,
 } from "./schema";
 
@@ -16,24 +15,6 @@ export const useFetch = async ({ url, method, data }: UseApiProps) => {
     method,
     data,
     baseURL: import.meta.env.VITE_VITE_API_ROOT,
-  });
-
-  return response.data;
-};
-
-export const userLogin = async (request: LoginDataRequest) => {
-  const response = await ApiClient.request({
-    method: "POST",
-    url: "/auth/signin",
-    data: request,
-  });
-  return response.data;
-};
-
-export const userInfo = async () => {
-  const response = await ApiClient.request({
-    method: "GET",
-    url: `/users`,
   });
 
   return response.data;
