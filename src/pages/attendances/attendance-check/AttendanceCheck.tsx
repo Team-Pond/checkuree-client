67483aefbd38e66864a1ea3253440fe79eacb6de
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-16 18:36:06 +0900

    Fix: 출석부 화면 컴포넌트화 작업

--- src/pages/attendances/attendance-check/AttendanceCheck.tsx
+++ src/pages/attendances/attendance-check/AttendanceCheck.tsx
@@ -1,20 +1,13 @@
+import Bottom from "./components/Bottom";
+import Header from "./components/Header";
+import MainContents from "./components/MainContent";
+
 export default function AttendanceCheck() {
   return (
-    <section>
-      <div>
-        <div>
-          <p>오후 2:00</p>
-          <div>
-            <p>배서윤</p>
-
-            <button className="">결석</button>
-            <button className="">출석</button>
-            <button className="w-8 h-8 ">
-              <img src="" alt="" />
-            </button>
-          </div>
-        </div>
-      </div>
+    <section className="flex flex-col w-full">
+      <Header />
+      <MainContents />
+      <Bottom />
     </section>
   );
 }
