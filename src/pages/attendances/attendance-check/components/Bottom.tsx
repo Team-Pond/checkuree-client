52f60d06bf58a659013b2790cf02bda6ac5e44e1
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-17 22:19:46 +0900

    Fix: BottomNavigation 상태작업

--- src/pages/attendances/attendance-check/components/Bottom.tsx
+++ src/pages/attendances/attendance-check/components/Bottom.tsx
@@ -1,21 +1,37 @@
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 export default function Bottom() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   const navigate = useNavigate();
   return (
     <div className="flex justify-between px-[44px] items-center  sticky bottom-0 z-50 w-full h-[92px] bg-white rounded-2xl">
       <div
         className="flex flex-col gap-2 items-center"
-        onClick={() => navigate("/attendances")}
+        onClick={() => navigate(currentPath)}
       >
         <img
-          src="/images/icons/attendance-check/ico-attendance.svg"
+          src={`/images/icons/attendance-check/${
+            currentPath.split("/")[1] === "attendances"
+              ? "ico-attendance-active"
+              : "ico-attendance"
+          }.svg`}
           alt="출석부 아이콘"
           width={20}
-          height={20}
         />
-        <p className="text-xs text-text-tertiary">출석부</p>
+        <p
+          className={twMerge(
+            "text-xs ",
+            currentPath.split("/")[1] === "attendances"
+              ? "text-text-primary"
+              : "text-text-tertiary"
+          )}
+        >
+          출석부
+        </p>
       </div>
       <div className="flex flex-col gap-2 items-center">
         <img
