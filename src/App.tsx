3e7b689ce443cf0df8c8922a7563c7e896c77133
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-20 16:02:04 +0900

    Update: 페이지 이동 시 스크롤 상단 작업

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,16 @@ import ProtectedRoute from "@/ProtectedRoute";
 import PageContainer from "@/components/PageContainer";
 import Loading from "@/components/Loading";
 import { lazy, Suspense } from "react";
+import ScrollToTop from "./components/ScrollToTop";
 
 // Lazy load components
 const Attendances = lazy(() => import("@/pages/attendances/Attendances"));
-const ListManagement = lazy(
-  () => import("@/pages/list-management/ListManagement")
+const AttendanceCreate = lazy(
+  () => import("@/pages/attendances/attendance-create/AttendanceCreate")
 );
 const KakaoSignIn = lazy(() => import("@/pages/kakao-auth/SignIn"));
 const CheckureeSignIn = lazy(() => import("@/pages/checkuree-auth/SignIn"));
+
 const SignIn = lazy(() => import("@/pages/auth/SignIn"));
 const AttendanceCheck = lazy(
   () => import("@/pages/attendances/attendance-check/AttendanceCheck")
@@ -28,8 +30,9 @@ interface RouteType {
 }
 const routes: RouteType[] = [
   { path: "/attendances", element: <Attendances /> }, // 출석부
+  { path: "/attendances/attendance-create", element: <AttendanceCreate /> }, // 출석부
   { path: "/attendances/:id", element: <AttendanceCheck /> },
-  { path: "/list-management/:id", element: <ListManagement /> },
+  { path: "/attendances/attendance-check", element: <AttendanceCheck /> },
 ];
 
 function App() {
@@ -37,20 +40,16 @@ function App() {
     <Router>
       <Suspense fallback={<Loading />}>
         <PageContainer>
+          <ScrollToTop />
           <Routes>
-            {/* / 경로 시 로그인 페이지로 이동 */}
             <Route path="/" element={<Navigate to={"/attendances"} />} />
+            <Route path="*" element={<Navigate to={""} />} /> {/* 404페이지 */}
             <Route path="/auth/signin" element={<SignIn />} />
             <Route
               path="/checkuree-auth/signin"
               element={<CheckureeSignIn />}
             />
             <Route path="/kakao-auth/signin" element={<KakaoSignIn />} />
-            <Route
-              path="/attendances/attendance-check"
-              element={<AttendanceCheck />}
-            />
-
             {/* 인증 처리 */}
             {routes.map((route) => {
               return (
