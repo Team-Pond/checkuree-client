a84baa6b11d68a4189b8c0ccb0f265ffe668cdb0
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-01-03 17:30:20 +0900

    style: import 경로 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RootToaster } from "./providers/RootToaster.tsx";
 import "dayjs/locale/ko"; // 한국어 locale 설정
 import dayjs from "dayjs";
+import App from "./App";
+import { RootToaster } from "./providers/RootToaster";
 dayjs.locale("ko");
 
 const queryClient = new QueryClient();
