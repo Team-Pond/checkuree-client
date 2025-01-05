d8bfabae884d34a715443818d6f3404de4297dbe
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-01-05 10:13:00 +0900

    feat: 출석부 생성 에러 핸들링

--- src/pages/books/book-create/BookCreate.tsx
+++ src/pages/books/book-create/BookCreate.tsx
@@ -9,6 +9,7 @@ import { useForm, useWatch } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { createBook } from "@/api v2/AttendanceBookApiClient";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 export default function BookCreate() {
   const navigate = useNavigate();
@@ -56,6 +57,15 @@ export default function BookCreate() {
     onSuccess: () => {
       toast.success("출석부를 생성하였습니다.");
       navigate("/book");
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        console.log(error.response?.data);
+        toast.error(error.response?.data?.data || "An error occurred");
+      } else {
+        console.log(error);
+        toast.error("An unexpected error occurred");
+      }
     },
   });
 
