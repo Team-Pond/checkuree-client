bb4bf74ced2f8befdb860429007ddf8414adfd4a
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-01-18 17:25:28 +0900

    feat: 출석 체크 api 추가(작업 필요)

--- src/pages/books/book-check/components/MainContent.tsx
+++ src/pages/books/book-check/components/MainContent.tsx
@@ -1,7 +1,14 @@
-import { useLocation } from "react-router-dom";
 import { statusCheckAttendee } from "@/api v2/AttendeeApiClient";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+
+import { ScheduleDataType } from "@/api v2/ScheduleSchema";
+import { scheduleCheckformatTime } from "@/utils";
+import {
+  updateRecordLesson,
+  updateRecordStatus,
+} from "@/api v2/RecordApiClient";
+import { useMutation } from "@tanstack/react-query";
 
 const MOCK_DATA = [
   {
@@ -36,83 +43,134 @@ const MOCK_DATA = [
   },
 ];
 
-export default function MainContents() {
-  const location = useLocation();
-  const attendanceBookId = location.pathname.split("/")[2];
+type IProps = {
+  bookSchedules: ScheduleDataType;
+  bookId: number;
+};
+export default function MainContents(props: IProps) {
+  const { bookId, bookSchedules } = props;
 
+  console.log(bookSchedules);
   const [attendees, setAttendees] = useState(MOCK_DATA);
 
   // TODO: 낙관적 업데이트 적용
   const checkAttendee = async (status: string, attendeeId: number) => {
     await statusCheckAttendee({
-      attendanceBookId,
+      attendanceBookId: bookId,
       attendeeId,
       status,
     });
   };
+
+  const { mutate: statusMutation } = useMutation({
+    mutationKey: [""],
+    mutationFn: async (status: string) =>
+      await updateRecordStatus({
+        params: {
+          attendanceBookId: Number(bookId!),
+          status,
+          scheduleId: 0,
+          recordId: 0,
+        },
+      }),
+    onSuccess: () => {},
+    onError: () => {},
+  });
+
+  const { mutate: lessonMutation } = useMutation({
+    mutationKey: [""],
+    mutationFn: async () =>
+      await updateRecordLesson({
+        params: {
+          attendanceBookId: Number(bookId!),
+          isTaught: true,
+          recordId: 0,
+        },
+      }),
+    onSuccess: () => {},
+    onError: () => {},
+  });
+
+  console.log(bookSchedules);
   return (
     <div className="w-full flex flex-col gap-4 justify-center items-center py-3 px-4 bg-bg-secondary scrollbar-hide custom-scrollbar-hide">
-      {attendees.map((attendee) => {
+      {bookSchedules?.content?.map((content, index) => {
         return (
           <div
-            key={attendee.id}
-            className="w-full text-left rounded-2xl bg-white px-6 pt-4 flex flex-col gap-3"
+            key={index}
+            className="w-full text-left rounded-2xl bg-white px-6 pt-1 flex flex-col"
           >
-            <p className=" font-bold text-sm text-text-secondary">
-              {attendee.time}
+            <p className="text-s-bold text-text-secondary h-12 flex items-center">
+              {scheduleCheckformatTime(content.startTime)}
             </p>
 
-            <div className="w-full h-[56px] flex items-center justify-between px-2 ">
-              <p className="font-bold  text-text-primary">{attendee.name}</p>
+            {content.schedules.map((schedule) => {
+              return (
+                <div className="w-full h-[56px] flex items-center justify-between px-2 ">
+                  <p className="font-bold  text-text-primary">
+                    {schedule.name}
+                  </p>
 
-              <div className="flex gap-4">
-                <div className="flex gap-2">
-                  {/* TODO: 결석은 status명이 어떻게 되는지? */}
-                  <button
-                    onClick={async () => await checkAttendee("", attendee.id)}
-                    className={twMerge(
-                      "rounded-lg text-sm w-[57px] h-[33px] flex items-center justify-center",
-                      attendee.status !== "ATTENDING"
-                        ? "bg-bg-destructive text-text-interactive-destructive"
-                        : "bg-bg-disabled text-text-disabled"
-                    )}
-                  >
-                    결석
-                  </button>
-                  <button
-                    onClick={async () =>
-                      await checkAttendee("ATTENDING", attendee.id)
-                    }
-                    className={twMerge(
-                      "rounded-lg text-sm w-[57px] h-[33px] flex items-center justify-center",
-                      attendee.status === "ATTENDING"
-                        ? "bg-bg-primary text-text-interactive-primary"
-                        : "bg-bg-disabled text-text-disabled"
-                    )}
-                  >
-                    출석
-                  </button>
+                  <div className="flex gap-4">
+                    <div className="flex gap-2">
+                      {/* TODO: 결석은 status명이 어떻게 되는지? */}
+                      <button
+                        onClick={
+                          () =>
+                            // await checkAttendee("", schedule.attendeeId)
+                            statusMutation("REJECTED")
+                          // APPROVED
+                        }
+                        className={twMerge(
+                          "rounded-lg text-sm w-[57px] h-[33px] flex items-center justify-center",
+                          schedule.recordStatus === "REJECTED"
+                            ? "bg-bg-destructive text-text-interactive-destructive"
+                            : "bg-bg-disabled text-text-disabled"
+                        )}
+                      >
+                        결석
+                      </button>
+                      <button
+                        onClick={
+                          () =>
+                            // await checkAttendee("", schedule.attendeeId)
+                            statusMutation("APPROVED")
+                          // APPROVED
+                        }
+                        className={twMerge(
+                          "rounded-lg text-sm w-[57px] h-[33px] flex items-center justify-center",
+                          schedule.recordStatus === "APPROVED"
+                            ? "bg-bg-primary text-text-interactive-primary"
+                            : "bg-bg-disabled text-text-disabled"
+                        )}
+                      >
+                        출석
+                      </button>
+                    </div>
+                    <button
+                      className={twMerge(
+                        "w-8 h-8 flex items-center justify-center rounded-lg",
+                        schedule.recordStatus === "ATTENDING"
+                          ? "bg-bg-tertiary"
+                          : "bg-bg-disabled"
+                      )}
+                      disabled={
+                        schedule.recordStatus === "ATTENDING" ? true : false
+                      }
+                    >
+                      <img
+                        src={`/images/icons/book-check/${
+                          schedule.recordStatus === "ATTENDING"
+                            ? "ico-note-active.svg"
+                            : "ico-note.svg"
+                        }`}
+                        alt=""
+                      />
+                    </button>
+                  </div>
                 </div>
-                <button
-                  className={twMerge(
-                    "w-8 h-8 flex items-center justify-center rounded-lg",
-                    attendee.status === "ATTENDING"
-                      ? "bg-bg-tertiary"
-                      : "bg-bg-disabled"
-                  )}
-                  disabled={attendee.status === "ATTENDING" ? true : false}
-                >
-                  <img
-                    src={`/images/icons/book-check/${
-                      attendee.status === "ATTENDING"
-                        ? "ico-note-active.svg"
-                        : "ico-note.svg"
-                    }`}
-                    alt=""
-                  />
-                </button>
-              </div>
-            </div>
+              );
+            })}
           </div>
         );
       })}
