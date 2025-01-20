d4635d57a531b0e19d5787982add064bde0f1b00
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-01-20 21:03:12 +0900

    fix: refresh 오류 수정

--- src/api v2/ApiClient.ts
+++ src/api v2/ApiClient.ts
@@ -46,15 +46,9 @@ function getRefreshToken(): string | undefined {
   return Cookies.get(import.meta.env.VITE_REFRESH_TOKEN);
 }
 
-let isRefreshing = false;
-
 async function refresh(config: AxiosRequestConfig) {
   const refreshToken = getRefreshToken();
-  if (isRefreshing) {
-    return Promise.reject(new Error("이미 토큰 갱신 중"));
-  }
 
-  isRefreshing = true;
   if (!!refreshToken) {
     try {
       const refreshResult = await axios.request({
