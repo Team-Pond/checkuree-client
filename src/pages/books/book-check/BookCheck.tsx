092a25ac29ddb942369ca93cbd57b065af42e1f8
Sang Hoo Park <sksk8922@gmail.com>
2025-01-26 01:43:41 +0900

    feat: checkedScheduleCount, totalScheduleCount 적용

--- src/pages/books/book-check/BookCheck.tsx
+++ src/pages/books/book-check/BookCheck.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import MainContents from "./components/MainContent";
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BookContext } from "@/context/BookContext";
 import { useParams, useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -9,6 +9,7 @@ import { getScheduleAttendee } from "@/api v2/ScheduleApiClient";
 import { ScheduleDataType } from "@/api v2/ScheduleSchema";
 import dayjs from "dayjs";
 import Bottom from "../components/Bottom";
+import { getScheduleCountOfDate } from "../../../api v2/ScheduleApiClient.ts";
 
 export default function BookCheck() {
   const context = useContext(BookContext);
@@ -18,6 +19,10 @@ export default function BookCheck() {
   const [searchParams] = useSearchParams();
   const bookName = searchParams.get("bookName");
   const [currentDate, setCurrentDate] = useState(dayjs()); // dayjs로 초기화
+  const [checkedScheduleCount, setCheckedScheduleCount] = useState<number>(0);
+  const [totalScheduleCount, setTotalScheduleCount] = useState<number>(0);
+
+  // const [checkedCount, setCheckedCount] = useState(0);
 
   const formattedDate = currentDate.format("YYYY-MM-DD"); // 데이터 값
 
@@ -30,6 +35,8 @@ export default function BookCheck() {
     setCurrentDate((prev) => prev.add(1, "day"));
   };
 
+  // 전체 데이터를 가지고 왔다고 가정
+  // checkedCount 상태 추가
   const { data: bookSchedules } = useQuery({
     queryKey: ["book-schedules", bookId, formattedDate],
     queryFn: async () =>
@@ -46,7 +53,36 @@ export default function BookCheck() {
       }),
   });
 
-  console.log(bookSchedules?.status === 200 && bookSchedules.data.content);
+  const { data: scheduleCount } = useQuery({
+    queryKey: ["schedule-count", bookId, formattedDate],
+    queryFn: async () =>
+      await getScheduleCountOfDate({
+        attendanceBookId: Number(bookId!),
+        params: {
+          date: formattedDate,
+        },
+      }),
+  });
+
+  // useEffect(() => {
+  //   if (scheduleCount?.data) {
+  //     setTotalScheduleCount(scheduleCount.data.totalCount);
+  //     setCheckedScheduleCount(scheduleCount.data.checkedCount);
+  //   }
+  // }, [scheduleCount]);
+
+  useEffect(() => {
+    if (bookSchedules?.data) {
+      setTotalScheduleCount(bookSchedules.data.numberOfElements);
+      const checkedCount = bookSchedules.data.content.reduce((total, schedules) => {
+        return total + schedules.schedules.filter((schedule) => schedule.recordStatus !== "PENDING").length;
+      }, 0);
+      setCheckedScheduleCount(checkedCount);
+    }
+  }, [scheduleCount]);
+
+  // console.log(bookSchedules?.status === 200 && bookSchedules.data.content);
+
   return (
     <section className="flex flex-col w-full scrollbar-hide custom-scrollbar-hide">
       <Header
@@ -56,11 +92,15 @@ export default function BookCheck() {
         handleNextDay={handleNextDay}
         currentDate={currentDate}
         formattedDate={formattedDate}
+        checkedScheduleCount={checkedScheduleCount}
+        totalScheduleCount={totalScheduleCount}
       />
       <MainContents
         bookSchedules={bookSchedules?.data as ScheduleDataType}
         currentDate={formattedDate}
         bookId={Number(bookId!)}
+        checkedScheduleCount={checkedScheduleCount}
+        setCheckedCount={setCheckedScheduleCount}
       />
       <div className="flex justify-between px-[44px] items-center w-full h-[92px] bg-bg-secondary" />
       <Bottom />
