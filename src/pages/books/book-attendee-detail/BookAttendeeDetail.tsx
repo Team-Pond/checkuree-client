18a6d38eeea46106ca8487577d9c19205f627f69
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-01-29 11:13:06 +0900

    feat: 출석부 디테일 추가

--- src/pages/books/book-attendee-detail/BookAttendeeDetail.tsx
+++ src/pages/books/book-attendee-detail/BookAttendeeDetail.tsx
@@ -1,8 +1,4 @@
 import CommonTabs from "@/components/Tabs";
-import StudentManage from "./components/StudentManage";
-import AttendanceManage from "./components/AttendanceManage";
-import LearningManage from "./components/LearningManage";
-import CounselManage from "./components/CounselManage";
 
 export default function BookAttendeeDetail() {
   return (
