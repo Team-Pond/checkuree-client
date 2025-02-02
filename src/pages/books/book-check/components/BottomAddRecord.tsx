3864a0cb9855eb111348ee77971b961594709014
Sang Hoo Park <sksk8922@gmail.com>
2025-02-03 01:28:02 +0900

    feat: 학생 정보 노출 수정

--- src/pages/books/book-check/components/BottomAddRecord.tsx
+++ src/pages/books/book-check/components/BottomAddRecord.tsx
@@ -2,6 +2,7 @@ import BottomDrawer from '../../../../components/BottomDrawer';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { searchAttendee } from '../../../../api v2/AttendeeApiClient';
+import { GenderType } from '../../../../api v2/AttendeeSchema';
 
 type IProps = {
   openFilter: boolean;
@@ -15,7 +16,7 @@ export const BottomAddRecord = (props:IProps) => {
   const [search, setSearch] = useState('');
   const [debouncedSearch, setDebouncedSearch] = useState('');
 
-// 디바운싱 적용
+  // 디바운싱 적용
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedSearch(search);
@@ -36,13 +37,9 @@ export const BottomAddRecord = (props:IProps) => {
           name: debouncedSearch,
         }).then((res) => res.data);
       }
-      return [];
     },
     enabled: debouncedSearch.length > 0, // 1글자 이상 입력되었을 때만 실행
   });
-  console.log(searchResults)
-
-
 
   return (
     <BottomDrawer
@@ -74,10 +71,30 @@ export const BottomAddRecord = (props:IProps) => {
 
             {/* 검색 결과 리스트 */}
             <div className="flex flex-col gap-2 max-h-[300px] overflow-y-auto">
-              {searchResults?.content?.length > 0 ? (
-                searchResults.content.map((student: { id: number; name: string }) => (
-                  <div key={student.id} className="p-2 border-b border-gray-200">
-                    {student.name}
+              {searchResults?.content?.length! > 0 ? (
+                searchResults!.content.map((student: { id: number; name: string,age:number,gender:GenderType }) => (
+                  <div
+                    key={student.id}
+                    className="py-1 px-2 flex gap-4"
+                    onClick={() =>{}}
+                  >
+                    <img
+                      src="/images/icons/book-roaster/ico-student.svg"
+                      alt="학생 아이콘"
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+                    <div className="flex items-center gap-2 text-left">
+                      <div className="flex gap-2">
+                        <p className="text-m-bold text-text-primary">
+                          {student.name}
+                        </p>
+                        <p className="text-m-semibold text-text-secondary">
+                          {student.age + '세'}
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 ))
               ) : (
