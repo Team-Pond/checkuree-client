fb237efcf33e793315881ecc039372196b57cadd
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-02-05 23:54:16 +0900

    chore: 사용하지 않는 코드 삭제

--- src/pages/checkuree-auth/queries.ts
+++ src/pages/checkuree-auth/queries.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation } from "@tanstack/react-query";
 import { LoginDataType } from "./SignIn";
 import axios from "axios";
 import { setTokens } from "@/lib/auth";
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { SignIn } from "@/api v2/AuthApiClient";
 
 export const useAuthLogin = () => {
+  const queryClient = new QueryClient();
   const navigate = useNavigate();
   return useMutation({
     mutationFn: async (params: LoginDataType) => await SignIn(params),
@@ -19,6 +20,7 @@ export const useAuthLogin = () => {
         accessToken: accessToken as string,
         refreshToken: refreshToken as string,
       });
+      queryClient.setQueryData(["user"], refreshToken);
       toast.success("로그인 되었습니다.");
       navigate("/");
     },
