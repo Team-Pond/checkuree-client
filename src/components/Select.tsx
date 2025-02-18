210edee6a4b273a389d7e76ff4e3e9809d83c187
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-02-18 11:41:46 +0900

    fix: Select style outline-none & rounded 작업

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -24,7 +24,7 @@ const RelationshipSelect = (props: IProps) => {
       onValueChange={(value) => onChangeGuardian("relationType", value)}
     >
       <Select.Trigger
-        className="inline-flex items-center justify-between w-[120px] px-3 py-2 bg-white border border-border-brand outline-none rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-green-500"
+        className="rounded-md outline-none inline-flex items-center justify-between w-[120px] px-3 py-2 bg-white border border-border-brand ounded-md text-sm focus:outline-none  focus:ring-green-500 "
         aria-label="관계"
       >
         <Select.Value placeholder="관계" />
@@ -33,7 +33,7 @@ const RelationshipSelect = (props: IProps) => {
         </Select.Icon>
       </Select.Trigger>
       <Select.Portal>
-        <Select.Content className="z-10 bg-white border border-gray-300 rounded-md shadow-md">
+        <Select.Content className="outline-none z-10 bg-white border border-gray-300 rounded-md shadow-md">
           <Select.ScrollUpButton className="flex items-center justify-center h-6 bg-white">
             <ChevronUpIcon />
           </Select.ScrollUpButton>
