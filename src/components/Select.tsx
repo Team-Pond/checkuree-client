e5009a2c1c4dbe2974387cf164d974e6559805ab
GanziMan <qjatn50089@gmail.com>
2025-02-21 08:05:19 +0900

    refactor: 재사용성을 위한 Select 수정정

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -6,28 +6,21 @@ import {
 } from "@radix-ui/react-icons";
 
 type IProps = {
-  onChangeGuardian: (key: string, value: string) => void;
+  onChange: (key: string, value: string) => void;
+  options: {
+    name: string;
+    value: string;
+  }[];
+  placeholder: string;
 };
 
-const options = [
-  { text: "모", value: "MOTHER" },
-  { text: "부", value: "FATHER" },
-  { text: "조부모", value: "GRANDPARENT" },
-  { text: "기타", value: "ETC" },
-];
-
-const RelationshipSelect = (props: IProps) => {
-  const { onChangeGuardian } = props;
+const CommonSelect = (props: IProps) => {
+  const { onChange, options, placeholder } = props;
 
   return (
-    <Select.Root
-      onValueChange={(value) => onChangeGuardian("relationType", value)}
-    >
-      <Select.Trigger
-        className="rounded-md outline-none inline-flex items-center justify-between w-[120px] px-3 py-2 bg-white border border-border-brand ounded-md text-sm focus:outline-none  focus:ring-green-500 "
-        aria-label="관계"
-      >
-        <Select.Value placeholder="관계" />
+    <Select.Root onValueChange={(value) => onChange("relationType", value)}>
+      <Select.Trigger className="rounded-md outline-none inline-flex items-center justify-between w-[120px] px-3 py-2 bg-white border border-border-brand ounded-md text-sm focus:outline-none  focus:ring-green-500 ">
+        <Select.Value placeholder={placeholder} />
         <Select.Icon>
           <ChevronDownIcon />
         </Select.Icon>
@@ -38,13 +31,13 @@ const RelationshipSelect = (props: IProps) => {
             <ChevronUpIcon />
           </Select.ScrollUpButton>
           <Select.Viewport className="p-2">
-            {options.map(({ text, value }) => (
+            {options.map(({ name, value }) => (
               <Select.Item
                 key={value}
                 value={value}
                 className="flex items-center p-2 rounded-md text-sm cursor-pointer hover:bg-gray-100"
               >
-                <Select.ItemText>{text}</Select.ItemText>
+                <Select.ItemText>{name}</Select.ItemText>
                 <Select.ItemIndicator className="ml-auto text-green-500">
                   <CheckIcon />
                 </Select.ItemIndicator>
@@ -60,4 +53,4 @@ const RelationshipSelect = (props: IProps) => {
   );
 };
 
-export default RelationshipSelect;
+export default CommonSelect;
