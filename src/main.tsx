ce0be3ad0b0260ffa5c1961089c45d27d87cb61b
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-02-23 20:22:23 +0900

    feat: HeltmetProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "dayjs/locale/ko"; // 한국어 locale 설정
 import dayjs from "dayjs";
 import App from "./App";
 import { RootToaster } from "./providers/RootToaster";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 dayjs.locale("ko");
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <HelmetProvider>
     <QueryClientProvider client={queryClient}>
       <RootToaster />
       <ReactQueryDevtools initialIsOpen={true} />
       <App />
     </QueryClientProvider>
-  </StrictMode>
+  </HelmetProvider>
 );
