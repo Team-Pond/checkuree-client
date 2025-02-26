3a4653b72371ec83a63318847c7cc060a3122b15
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-02-26 19:48:46 +0900

    refactor: ProgressModal 작업

--- src/pages/books/book-attendee-detail/components/NextProgressModal.tsx
+++ src/pages/books/book-attendee-detail/components/NextProgressModal.tsx
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { useBookCourses, useProgressPromote } from "../queries";
+import React from "react";
+import { useBookCourses } from "../queries";
+import useProgressFormStore from "@/store/progressStore";
 
 interface Props {
-  onClose: () => void;
-  attendeeProgressId: number;
   bookId: number;
 }
 
-const NextProgressModal: React.FC<Props> = ({
-  onClose,
-  bookId,
-  attendeeProgressId,
-}) => {
-  const { attendeeId } = useParams();
-  const [formData, setFormData] = useState({
-    completeAt: "",
-    startAt: "",
-    nextGradeId: "",
-  });
-
+const NextProgressModal: React.FC<Props> = ({ bookId }) => {
+  const { formData, updateFormData } = useProgressFormStore();
   const { data: bookCourses } = useBookCourses({
     bookId: String(bookId),
     openDrawer: true, // 모달이 열렸으니 true 처리
@@ -31,7 +19,10 @@ const NextProgressModal: React.FC<Props> = ({
     totalBookGrades.push(...course.grades);
   });
 
-  const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDateChange = (
+    key: "completeAt" | "startAt",
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     let input = e.target.value.replace(/\D/g, "");
 
     if (input.length > 8) {
@@ -45,49 +36,8 @@ const NextProgressModal: React.FC<Props> = ({
     if (input.length >= 8) {
       input = input.slice(0, 7) + "." + input.slice(7);
     }
-
-    setFormData((prev) => ({
-      ...prev,
-      startAt: input,
-    }));
+    updateFormData(key, input);
   };
-
-  const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let input = e.target.value.replace(/\D/g, "");
-
-    if (input.length > 8) {
-      input = input.slice(0, 8);
-    }
-
-    if (input.length >= 5) {
-      input = input.slice(0, 4) + "." + input.slice(4);
-    }
-
-    if (input.length >= 8) {
-      input = input.slice(0, 7) + "." + input.slice(7);
-    }
-
-    setFormData((prev) => ({
-      ...prev,
-      completeAt: input,
-    }));
-  };
-
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setFormData((prev) => ({
-      ...prev,
-      nextGradeId: e.target.value,
-    }));
-  };
-
-  // TODO: mutation 후 데이터 최신작업 필요
-  const { mutate: progressMutation } = useProgressPromote({
-    bookId,
-    attendeeProgressId,
-    formData,
-    attendeeId: Number(attendeeId),
-    onClose,
-  });
 
   return (
     <div className="flex flex-col gap-6 justify-center items-center w-full">
@@ -101,7 +51,7 @@ const NextProgressModal: React.FC<Props> = ({
           placeholder="YYYY.MM.DD"
           className="outline-none bg-white border border-[#E7E7E7] rounded-xl w-full h-12 flex items-center pl-4"
           value={formData.completeAt}
-          onChange={handleEndDateChange}
+          onChange={(e) => handleDateChange("completeAt", e)}
         />
       </div>
       <div className="flex flex-col gap-2 w-full">
@@ -111,7 +61,7 @@ const NextProgressModal: React.FC<Props> = ({
         </div>
         <select
           value={formData.nextGradeId}
-          onChange={handleSelectChange}
+          onChange={(e) => updateFormData("nextGradeId", e.target.value)}
           className="outline-none bg-white border border-[#E7E7E7] rounded-xl w-full h-12 pl-4 text-m-medium"
         >
           <option value="">과정을 선택하세요</option>
@@ -134,25 +84,8 @@ const NextProgressModal: React.FC<Props> = ({
           placeholder="YYYY.MM.DD"
           className="outline-none bg-white border border-[#E7E7E7] rounded-xl w-full h-12 flex items-center pl-4"
           value={formData.startAt}
-          onChange={handleStartDateChange}
+          onChange={(e) => handleDateChange("startAt", e)}
         />
-      </div>
-
-      <div className="flex gap-4 w-full">
-        <button
-          type="button"
-          onClick={onClose}
-          className="w-full h-12 flex justify-center items-center rounded-2xl bg-bg-secondary text-text-secondary text-l-semibold"
-        >
-          취소
-        </button>
-        <button
-          type="button"
-          className="w-full h-12 flex justify-center items-center rounded-2xl bg-bg-tertiary text-[#F1F8F3] text-l-semibold"
-          onClick={() => progressMutation()}
-        >
-          저장하기
-        </button>
       </div>
     </div>
   );
