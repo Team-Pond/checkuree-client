0d360401c6a8b67bd5550e54d5abea43bd63d438
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-05 08:12:03 +0900

    feat: 상담 생성 기능 개선 및 스키마 수정

--- src/pages/books/book-attendee-detail/_schema.ts
+++ src/pages/books/book-attendee-detail/_schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const CounsellingSchema = z.object({
-  counsellingType: z.enum(["PHONE", "VISIT", "KAKAOTALK"]),
-  counsellingTopicTypes: z.array(
-    z.enum(["FUTURE_PATH", "STUDY_PROGRESS", "PEER_RELATIONS", "ETC"]),
+  type: z.enum(["PHONE", "VISIT", "KAKAOTALK"]),
+  topics: z.array(
+    z.enum(["FUTURE_PATH", "STUDY_PROGRESS", "PEER_RELATIONS", "ETC"])
   ),
-  counsellingDate: z.string().nonempty({ message: "상담일시는 필수입니다." }),
+  counsellingAt: z.string().nonempty({ message: "상담일시는 필수입니다." }),
   description: z.string(),
   counseleeId: z.number().positive({ message: "상담자는 필수입니다." }),
 });
