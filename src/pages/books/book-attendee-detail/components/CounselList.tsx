bb4f3852f5fd58818502012a2a7ffae75fe355ee
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-05 19:04:22 +0900

    fix: CounselList 컴포넌트에서 UI 개선 및 상담자 정보 추가

--- src/pages/books/book-attendee-detail/components/CounselList.tsx
+++ src/pages/books/book-attendee-detail/components/CounselList.tsx
@@ -32,48 +32,55 @@ const CounselList = (args: IProps) => {
       <p className="flex text-s-bold text-[#5d5d5d]">
         상담 내역 <img src="" alt="" />
       </p>
-      <p className="text-m-semibold text-text-primary">
-        {format(counsellingAt, "yyyy.MM.dd")}
-      </p>
-      <div className="flex flex-col space-y-1">
-        <div className="flex items-center text-sm">
-          <p className="text-text-tertiary w-24">유형</p>
-          <p className="text-text-primary">
-            {counsellingTypeToKor(counselType)}
-          </p>
-        </div>
-        <div className="flex flex-row text-sm">
-          <p className="text-text-tertiary w-24 items-baseline">주제</p>
-          <div className="flex flex-col">
-            {counselSubjects
-              .reduce<CounsellingTopicType[][]>((acc, cur) => {
-                if (!acc.length || acc[acc.length - 1].join(", ").length >= 17)
-                  acc.push([]); // 길이가 17이 넘어가는 경우 줄 띄우기
-                acc[acc.length - 1].push(cur);
-                return acc;
-              }, [])
-              .map((subjects) => {
-                return (
-                  <p className="break-keep text-left">
-                    {subjects
-                      .map((subject) => counsellingTopicTypeToKor(subject))
-                      .join(", ")}
-                  </p>
-                );
-              })}
+      <div className="flex flex-col gap-2">
+        <p className="text-m-semibold text-text-primary">
+          {format(counsellingAt, "yyyy.MM.dd")}
+        </p>
+        <div className="flex flex-col space-y-1">
+          <div className="flex items-center text-sm">
+            <p className="text-text-tertiary w-24">유형</p>
+            <p className="text-text-primary text-s-semibold">
+              {counsellingTypeToKor(counselType)}
+            </p>
           </div>
-        </div>
-        <div className="flex items-center text-sm">
-          <p className="text-text-tertiary w-24">상담자</p>
-          <p className="text-text-primary">
-            {relationTypeToKor(counselleeType)}
-          </p>
+          <div className="flex flex-row text-sm">
+            <p className="text-text-tertiary w-24 items-baseline">주제</p>
+            <div className="flex flex-col">
+              {counselSubjects
+                .reduce<CounsellingTopicType[][]>((acc, cur) => {
+                  if (
+                    !acc.length ||
+                    acc[acc.length - 1].join(", ").length >= 17
+                  )
+                    acc.push([]); // 길이가 17이 넘어가는 경우 줄 띄우기
+                  acc[acc.length - 1].push(cur);
+                  return acc;
+                }, [])
+                .map((subjects) => {
+                  return (
+                    <p className="break-keep text-left text-s-semibold">
+                      {subjects
+                        .map((subject) => counsellingTopicTypeToKor(subject))
+                        .join(", ")}
+                    </p>
+                  );
+                })}
+            </div>
+          </div>
+          <div className="flex items-center text-sm">
+            <p className="text-text-tertiary w-24">상담자</p>
+            <p className="text-text-primary text-s-semibold">
+              {relationTypeToKor(counselleeType)}
+            </p>
+          </div>
         </div>
       </div>
       <div className="fixed bottom-11 ml-[250px] max-w-[390px] w-full">
         <button
           onClick={() => {
-            navigate(`/book/${bookId}/attendee/${attendeeId}/counselling`);
+            navigate(`/book/${bookId}/attendee/${attendeeId}/counselling`, {
+              state: {},
+            });
           }}
           className="w-[90px] h-[46px] rounded-full flex gap-2 justify-center items-center bg-bg-tertiary"
         >
