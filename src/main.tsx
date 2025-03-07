cc42bf67d9eeae5c1250c5fb15d04ab884966184
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-07 17:44:53 +0900

    feat: implement server-side rendering with ReactDOM.hydrateRoot in main.tsx and add key prop to Calendar component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,14 +15,32 @@ dayjs.locale("ko");
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <RootToaster />
-      <ReactQueryDevtools initialIsOpen={true} />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </HelmetProvider>
-);
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RootToaster />
+        <ReactQueryDevtools initialIsOpen={true} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </HelmetProvider>
+  );
+} else {
+  root.render(
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RootToaster />
+        <ReactQueryDevtools initialIsOpen={true} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </HelmetProvider>
+  );
+}
