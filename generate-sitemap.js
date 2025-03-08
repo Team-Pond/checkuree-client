5b2a02b5be7e7b042da92e46518f5c57d4afd954
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-08 20:20:43 +0900

    feat: generate sitemap and update package scripts

---
+++ generate-sitemap.js
@@ -0,0 +1,33 @@
+import { SitemapStream } from "sitemap";
+import { createWriteStream, existsSync, mkdirSync } from "fs";
+
+// dist 폴더가 없으면 생성
+if (!existsSync("./dist")) {
+  mkdirSync("./dist");
+}
+
+const sitemap = new SitemapStream({
+  hostname: "https://pond-client.vercel.app/",
+});
+const writeStream = createWriteStream("./dist/sitemap.xml");
+
+// sitemap 스트림을 writeStream으로 pipe
+sitemap.pipe(writeStream);
+
+// sitemap에 경로 추가
+sitemap.write({ url: "//auth/signin", changefreq: "daily", priority: 1 });
+
+sitemap.write({
+  url: "/kakao-auth/signin",
+  changefreq: "weekly",
+  priority: 0.9,
+});
+sitemap.write({ url: "/book", changefreq: "weekly", priority: 0.8 });
+sitemap.write({ url: "/book/create", changefreq: "monthly", priority: 0.7 });
+
+sitemap.end();
+
+// writeStream이 완료될 때까지 기다림
+writeStream.on("finish", () => {
+  console.log("Sitemap generated successfully!");
+});
