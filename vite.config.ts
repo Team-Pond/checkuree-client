e6b90f14b3787389ca189daff7629279ea13541b
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-10 07:57:53 +0900

    fix: update port configuration to 443 and remove prerender plugin from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,10 @@ import tailwindcss from "tailwindcss";
 import svgr from "@svgr/rollup";
 import path from "path";
 import createSitemap from "vite-plugin-sitemap";
-import prerender from "@prerenderer/rollup-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
-    prerender({
-      routes: ["/", "/auth/signin", "/kakao-auth/signin"],
-      renderer: "@prerenderer/renderer-puppeteer",
-      server: {
-        port: 5173,
-        host: "localhost",
-      },
-      rendererOptions: {
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      },
-      postProcess(renderedRoute) {
-        renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, "https:")
-          .replace(
-            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
-            "https://pond-client.vercel.app/"
-          );
-      },
-    }),
     tsconfigPaths(),
     svgr(),
     createSitemap({
