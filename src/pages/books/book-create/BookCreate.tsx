b33644382a0c361ec9680c80b904a8beb2487df9
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-22 23:52:10 +0900

    feat: BookCreate 페이지에 FormHeader 컴포넌트 추가 및 스타일 수정

--- src/pages/books/book-create/BookCreate.tsx
+++ src/pages/books/book-create/BookCreate.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { bookSchema, CreateBookSchema } from "./_schema";
 import SEO from "@/components/SEO";
 import { CourseData } from "@/api/type";
+import FormHeader from "./components/FormHeader";
 
 export default function BookCreate() {
   const navigate = useNavigate();
@@ -68,7 +69,7 @@ export default function BookCreate() {
         content="체쿠리 음악학원 출석부 서비스의 출석부 등록 페이지입니다."
       />
       <form
-        className="flex flex-col gap-7 w-full pb-[30px]"
+        className="flex flex-col gap-10 w-full pb-[30px]"
         onSubmit={handleSubmit(() => {
           courseCreateParam.length > 0 &&
             bookMutation({
@@ -81,28 +82,8 @@ export default function BookCreate() {
             });
         })}
       >
-        <div className="w-full h-[64px] flex items-center justify-between px-4 py-5">
-          <p className="font-bold text-text-primary text-[22px]">출석부 등록</p>
-          <img
-            src="/images/icons/book-create/ico-close.svg"
-            alt="닫기 아이콘"
-            width={32}
-            height={32}
-            onClick={() => navigate("/book")}
-          />
-        </div>
-
+        <FormHeader isStep2={isStep2} />
         <div className="w-full flex flex-col gap-10 items-center">
-          <div className="flex gap-2 w-full justify-center">
-            <hr className="border-[2px] border-bg-tertiary max-w-[174px] w-full rounded-full" />
-            <div
-              className={twMerge(
-                "border-[2px]  max-w-[174px] w-full rounded-full",
-                isStep2 ? "border-bg-tertiary" : "border-[#DDEEDF]"
-              )}
-            />
-          </div>
-
           <div className="flex w-full justify-center">
             <div className="flex flex-col justify-center gap-6 max-w-[342px] w-full">
               {isStep2 ? (
