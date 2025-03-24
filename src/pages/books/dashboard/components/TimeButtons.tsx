a648e93150f24d28d74dc6f423f388d8b48057bf
GanziMan <qjatn50089@gmail.com>
2025-03-25 01:00:09 +0900

    Feature/qa 0323 (#151)
    * feat: DonutChart 컴포넌트에서 총 데이터 수 표시 방식 수정
    
    * feat: AttendanceRateChart 및 TimeButtons 컴포넌트 리팩토링 및 최적화

--- src/pages/books/dashboard/components/TimeButtons.tsx
+++ src/pages/books/dashboard/components/TimeButtons.tsx
@@ -1,67 +1,65 @@
+import { PeriodType } from "@/api/type";
 import { twMerge } from "tailwind-merge";
 
-type TimePeriod = "DAILY" | "WEEKLY" | "MONTHLY";
-interface IProps {
-  timeStatus: TimePeriod;
-  handleTimeStatus: (timeStatus: TimePeriod) => void;
-}
+type IProps = {
+  timeStatus: PeriodType;
+  handleTimeStatus: (timeStatus: PeriodType) => void;
+};
+const BUTTON_OPTIONS = [
+  {
+    status: "DAILY",
+    type: "일간",
+  },
+  {
+    status: "WEEKLY",
+    type: "주간",
+  },
+  {
+    status: "MONTHLY",
+    type: "월간",
+  },
+];
 export default function TimeButtons({ timeStatus, handleTimeStatus }: IProps) {
   return (
     <div className="flex">
-      <button
-        onClick={() => handleTimeStatus("DAILY")}
-        className={twMerge(
-          "w-[57px] h-[33px] rounded-lg",
-          timeStatus === "DAILY" ? "bg-bg-interactive-primary" : ""
-        )}
-      >
-        <span
-          className={twMerge(
-            "text-[14px]",
-            timeStatus === "DAILY"
-              ? "text-text-interactive-primary-press"
-              : "text-text-interactive-secondary"
-          )}
-        >
-          일간
-        </span>
-      </button>
-      <button
-        onClick={() => handleTimeStatus("WEEKLY")}
-        className={twMerge(
-          "w-[57px] h-[33px]  rounded-lg",
-          timeStatus === "WEEKLY" ? "bg-bg-interactive-primary" : ""
-        )}
-      >
-        <span
-          className={twMerge(
-            "text-[14px]",
-            timeStatus === "WEEKLY"
-              ? "text-text-interactive-primary-press"
-              : "text-text-interactive-secondary"
-          )}
-        >
-          주간
-        </span>
-      </button>
-      <button
-        onClick={() => handleTimeStatus("MONTHLY")}
-        className={twMerge(
-          "w-[57px] h-[33px] rounded-lg",
-          timeStatus === "MONTHLY" ? "bg-bg-interactive-primary" : ""
-        )}
-      >
-        <span
-          className={twMerge(
-            "text-[14px]",
-            timeStatus === "MONTHLY"
-              ? "text-text-interactive-primary-press"
-              : "text-text-interactive-secondary"
-          )}
-        >
-          월간
-        </span>
-      </button>
+      {BUTTON_OPTIONS.map((option) => {
+        return (
+          <Button
+            timeStatus={timeStatus}
+            handleTimeStatus={handleTimeStatus}
+            status={option.status as PeriodType}
+            type={option.type}
+          />
+        );
+      })}
     </div>
+  );
+}
+
+function Button({
+  timeStatus,
+  handleTimeStatus,
+  status,
+  type,
+}: IProps & { status: PeriodType; type: string }) {
+  return (
+    <button
+      onClick={() => handleTimeStatus(status)}
+      className={twMerge(
+        "w-[57px] h-[33px] rounded-lg",
+        timeStatus === status ? "bg-bg-interactive-primary" : ""
+      )}
+    >
+      <span
+        className={twMerge(
+          "text-[14px]",
+          timeStatus === status
+            ? "text-text-interactive-primary-press"
+            : "text-text-interactive-secondary"
+        )}
+      >
+        {type}
+      </span>
+    </button>
   );
 }
