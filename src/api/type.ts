6c7b5bb3703c841aab484de8127663074ef9a20f
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-26 02:37:13 +0900

    feat: AttendeeStatisticsType에 요일 타입 추가 및 한글 매핑 추가

--- src/api/type.ts
+++ src/api/type.ts
@@ -206,8 +206,27 @@ export type AttendeeStatisticsType = "CURRICULUM" | "AGE" | "DAY";
 
 export type AttendeeStatisticType = {
   type: AttendeeStatisticsType;
-  content: {
+  contents: {
     name: string;
     count: number;
   }[];
+};
+
+export type DayOfWeek =
+  | "MONDAY"
+  | "TUESDAY"
+  | "WEDNESDAY"
+  | "THURSDAY"
+  | "FRIDAY"
+  | "SATURDAY"
+  | "SUNDAY";
+
+export const dayMap: Record<DayOfWeek, string> = {
+  MONDAY: "월",
+  TUESDAY: "화",
+  WEDNESDAY: "수",
+  THURSDAY: "목",
+  FRIDAY: "금",
+  SATURDAY: "토",
+  SUNDAY: "일",
 };
