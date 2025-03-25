c170208d253b6e84de551b883ee8017c56055d87
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-26 02:37:47 +0900

    feat: CategoryButtons 컴포넌트 추가 및 통계 상태 변경 기능 구현

---
+++ src/pages/books/dashboard/components/CategoryButtons.tsx
@@ -0,0 +1,69 @@
+import { AttendeeStatisticsType, PeriodType } from "@/api/type";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type IProps = {
+  timeStatus: AttendeeStatisticsType;
+  handleTimeStatus: (timeStatus: AttendeeStatisticsType) => void;
+};
+const BUTTON_OPTIONS = [
+  {
+    status: "DAY",
+    type: "요일",
+  },
+  {
+    status: "AGE",
+    type: "나이",
+  },
+  {
+    status: "CURRICULUM",
+    type: "커리큘럼",
+  },
+];
+function CategoryButtons({ timeStatus, handleTimeStatus }: IProps) {
+  return (
+    <div className="flex">
+      {BUTTON_OPTIONS.map((option) => {
+        return (
+          <Button
+            key={option.status}
+            timeStatus={timeStatus}
+            handleTimeStatus={handleTimeStatus}
+            status={option.status as AttendeeStatisticsType}
+            type={option.type}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default React.memo(CategoryButtons);
+
+function Button({
+  timeStatus,
+  handleTimeStatus,
+  status,
+  type,
+}: IProps & { status: AttendeeStatisticsType; type: string }) {
+  return (
+    <button
+      onClick={() => handleTimeStatus(status)}
+      className={twMerge(
+        "px-4 h-[33px] rounded-lg",
+        timeStatus === status ? "bg-bg-interactive-primary" : ""
+      )}
+    >
+      <span
+        className={twMerge(
+          "text-[14px]",
+          timeStatus === status
+            ? "text-text-interactive-primary-press"
+            : "text-text-interactive-secondary"
+        )}
+      >
+        {type}
+      </span>
+    </button>
+  );
+}
