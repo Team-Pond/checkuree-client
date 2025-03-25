f12d5990c8ffd9e4de828e8d8235902b4dfdcf2c
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-03-26 02:37:38 +0900

    feat: AttendanceRateChart에서 출석률 통계 항목 추가 및 시각화 개선

--- src/pages/books/dashboard/components/AttendanceRateChart.tsx
+++ src/pages/books/dashboard/components/AttendanceRateChart.tsx
@@ -9,6 +9,12 @@ const TODAY_TEXT = "오늘의 출석률";
 const WEEKLY_TEXT = "이번 주 출석률";
 const MONTHLY_TEXT = "이번 달의 출석률";
 
+type Statistic = {
+  label: string;
+  rate: number;
+  bgColor: string;
+  textColor: string;
+};
 interface IProps {
   statisticData: StatisticType;
   tabChange: (tab: PeriodType) => void;
@@ -28,8 +34,45 @@ function AttendanceRateChart({ statisticData, tabChange, tab }: IProps) {
     }
   };
 
+  const statistics: Statistic[] = [
+    {
+      label: "출석",
+      rate: statisticData.attendRate,
+      bgColor: "bg-border-brand",
+      textColor: "text-text-brand",
+    },
+    {
+      label: "보강",
+      rate: statisticData.makeupRate,
+      bgColor: "bg-[#F2BD2D]",
+      textColor: "text-[#EC9E14]",
+    },
+    {
+      label: "결석",
+      rate: statisticData.absentRate,
+      bgColor: "bg-[#EA5353]",
+      textColor: "text-[#EA5353]",
+    },
+    {
+      label: "미출석",
+      rate: statisticData.unCheckedRate,
+      bgColor: "bg-[#E7E7E7]",
+      textColor: "text-[#B0B0B0]",
+    },
+  ];
+
+  const StatisticItem = ({ label, rate, bgColor, textColor }: Statistic) => (
+    <div className="flex gap-2 items-center">
+      <div className="flex items-center gap-[6px]">
+        <Squre bg={bgColor} />
+        <p className="text-xs-medium">{label}</p>
+      </div>
+      <Ratio color={textColor}>{rate}%</Ratio>
+    </div>
+  );
+
   return (
-    <Fragment>
+    <div className="flex flex-col gap-2">
       <ButtonsWrapper>
         <div className="flex flex-col text-left space-y-[-3px]">
           <p className="text-m-bold">{attendanceTextChange(tab)}</p>
@@ -43,42 +86,38 @@ function AttendanceRateChart({ statisticData, tabChange, tab }: IProps) {
         />
       </ButtonsWrapper>
       <ChartWrapper>
-        <DonutChart
-          data={[
-            statisticData.attendCount,
-            statisticData.makeupCount,
-            statisticData.absentCount,
-          ]}
-          labels={labels}
-        />
-        <div className="flex gap-[10px]">
-          <div className="flex gap-2 items-center">
-            <div className="flex items-center gap-[6px]">
-              <Squre bg="bg-border-brand" />
-              <p className="text-xs-medium">출석</p>
-            </div>
-            <Ratio color="text-text-brand">{statisticData.attendRate}%</Ratio>
+        <div className="flex gap-3">
+          <div>
+            <p className="text-xs-medium text-text-interactive-secondary text-left">
+              단위: 명
+            </p>
+            <DonutChart
+              data={[
+                statisticData.attendCount,
+                statisticData.makeupCount,
+                statisticData.absentCount,
+                statisticData.unCheckedCount,
+              ]}
+              labels={labels}
+            />
           </div>
-          <div className="flex gap-2 items-center">
-            <div className="flex items-center gap-[6px]">
-              <Squre bg="bg-[#F2BD2D]" />
-              <p className="text-xs-medium">보강</p>
-            </div>
-            <Ratio color="text-[#EC9E14]">{statisticData.makeupRate}%</Ratio>
-          </div>
-          <div className="flex gap-2 items-center">
-            <div className="flex items-center gap-[6px]">
-              <Squre bg="bg-[#EA5353]" />
-              <p className="text-xs-medium">결석</p>
-            </div>
-            <Ratio color="text-[#EA5353]">{statisticData.absentRate}%</Ratio>
-          </div>
+          <StatisticItemWrapper>
+            {statistics.map((stat) => (
+              <StatisticItem
+                key={stat.label}
+                label={stat.label}
+                rate={stat.rate}
+                bgColor={stat.bgColor}
+                textColor={stat.textColor}
+              />
+            ))}
+          </StatisticItemWrapper>
         </div>
-        <p className="mt-4 text-s-semibold">
+        <Summation>
           오늘은 약 {statisticData.attendRate}%의 학생들이 출석했어요.
-        </p>
+        </Summation>
       </ChartWrapper>
-    </Fragment>
+    </div>
   );
 }
 
@@ -90,5 +129,7 @@ const Ratio = tw.div`${({ color }: { color: string }) =>
 
 const ChartWrapper = tw.div`flex flex-col items-center gap-2 w-full h-[284px] bg-white rounded-2xl mx-auto py-4`;
 const ButtonsWrapper = tw.div`flex justify-between w-full items-center`;
+const StatisticItemWrapper = tw.p`flex flex-col gap-2px`;
+const Summation = tw.p`flex flex-col gap-2px`;
 
 export default React.memo(AttendanceRateChart);
