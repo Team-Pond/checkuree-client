ecd4bed28dcbd2771c2827795ca65f6653a7a2e8
GanziMan <87753974+GanziMan@users.noreply.github.com>
2025-04-02 16:11:02 +0900

    Qa/dashboard date format (#159)
    * fix: 타입 정의에서 세미콜론을 제거하여 일관성 유지
    
    * fix: 대시보드에서 날짜 계산 로직 수정 및 출석률 차트에 현재 날짜 전달

--- src/api/RecordSchema.ts
+++ src/api/RecordSchema.ts
@@ -1,80 +1,80 @@
-import { AttendeeRecord, StatisticType, STATUS } from "./type";
+import { AttendeeRecord, StatisticType, STATUS } from './type'
 
 type ResponseBase = {
-  message: string;
-};
+  message: string
+}
 
 type ErrorResponse = ResponseBase & {
-  status: 400 | 500 | 403;
-  data: {};
-  timeStamp: string;
-};
+  status: 400 | 500 | 403
+  data: {}
+  timeStamp: string
+}
 
 export type UpdateRecordResponse =
   | ({
-      status: 200;
+      status: 200
       data: {
-        id: number;
-      };
+        id: number
+      }
     } & ResponseBase)
-  | ErrorResponse;
+  | ErrorResponse
 
 export type UpdateRecordRequest = {
-  recordId: number;
-  attendanceBookId: number;
-  scheduleId?: number;
-  status?: STATUS | string;
-  attendTime?: string;
-};
+  recordId: number
+  attendanceBookId: number
+  scheduleId?: number
+  status?: STATUS | string
+  attendTime?: string
+}
 
 export type UpdateRecordLessonResponse =
   | ({
-      status: 200;
+      status: 200
       data: {
-        id: number;
-      };
+        id: number
+      }
     } & ResponseBase)
-  | ErrorResponse;
+  | ErrorResponse
 
 export type UpdateRecordLessonRequest = {
-  recordId: number;
-  attendanceBookId: number;
-  isTaught: boolean;
-};
+  recordId: number
+  attendanceBookId: number
+  isTaught: boolean
+}
 
 export type UpdateRecordAllResponse =
   | ({
-      status: 200;
+      status: 200
       data: {
-        affectedRows: number;
-      };
+        affectedRows: number
+      }
     } & ResponseBase)
-  | ErrorResponse;
+  | ErrorResponse
 
 export type UpdateRecordAllRequest = {
-  attendDate: string;
-  attendanceBookId: number;
-};
+  attendDate: string
+  attendanceBookId: number
+}
 
 export type CreateRecordResponse =
   | ({
-      status: 200;
+      status: 200
       data: {
-        id: number;
-      };
+        id: number
+      }
     } & ResponseBase)
-  | ErrorResponse;
+  | ErrorResponse
 
 // attendTime의 형태를 정의하는 타입
 
 export type CreateRecordRequest = {
-  attendanceBookId: number;
-  attendeeId: number;
-  scheduleId?: number;
-  attendDate: string; // "YYYY-MM-DD" 형태의 문자열로 전송
-  attendTime: string;
-  status: STATUS;
-};
+  attendanceBookId: number
+  attendeeId: number
+  scheduleId?: number
+  attendDate: string // "YYYY-MM-DD" 형태의 문자열로 전송
+  attendTime: string
+  status: STATUS
+}
 
 /**
  * 입력한 기간의 출석대상의 출석부 기록을 조회합니다.
@@ -82,39 +82,39 @@ export type CreateRecordRequest = {
  * @param to 조회 종료일 yyyy-MM-dd 형식 문자열
  */
 export type GetAttendeeRecordsRequest = {
-  from: string;
-  to: string;
-};
+  from: string
+  to: string
+}
 
 export type GetAttendeeRecordsResponse =
   | {
-      status: 200;
-      data: AttendeeRecord[];
+      status: 200
+      data: AttendeeRecord[]
     }
-  | ErrorResponse;
+  | ErrorResponse
 
 export type DeleteRecordRequest = {
-  attendanceBookId: number;
-  recordId: number;
-};
+  attendanceBookId: number
+  recordId: number
+}
 export type DeleteRecordResponse =
   | {
-      status: 200;
-      message: string;
-      data: {};
+      status: 200
+      message: string
+      data: {}
     }
-  | ErrorResponse;
+  | ErrorResponse
 
 export type GetStatisticsRequest = {
-  from: string;
-  to: string;
-  periodType: "DAILY" | "WEEKLY" | "MONTHLY";
-};
+  from: string
+
+  periodType: 'DAILY' | 'WEEKLY' | 'MONTHLY'
+}
 
 export type GetStatisticsResponse =
   | {
-      status: 200;
-      message: string;
-      data: StatisticType;
+      status: 200
+      message: string
+      data: StatisticType
     }
-  | ErrorResponse;
+  | ErrorResponse
--- src/pages/books/dashboard/components/AttendanceRateChart.tsx
+++ src/pages/books/dashboard/components/AttendanceRateChart.tsx
@@ -1,42 +1,38 @@
-import React from "react";
-import TimeButtons from "./TimeButtons";
+import React from 'react'
+import TimeButtons from './TimeButtons'
 
-import tw from "tailwind-styled-components";
-import { PeriodType } from "@/api/type";
-import { statisticPeriodText } from "@/utils";
-import { GetStatisticsResponse } from "@/api/RecordSchema";
-import RateChart from "./RateChart";
-
-const TODAY_TEXT = "오늘의 출석률";
-const WEEKLY_TEXT = "이번 주 출석률";
-const MONTHLY_TEXT = "이번 달의 출석률";
+import tw from 'tailwind-styled-components'
+import { PeriodType } from '@/api/type'
+import { statisticPeriodText } from '@/utils'
+import { GetStatisticsResponse } from '@/api/RecordSchema'
+import RateChart from './RateChart'
+import { Dayjs } from 'dayjs'
 
 interface IProps {
-  statisticData: GetStatisticsResponse;
-  tabChange: (tab: PeriodType) => void;
-  tab: PeriodType;
+  statisticData: GetStatisticsResponse
+  tabChange: (tab: PeriodType) => void
+  tab: PeriodType
+  currentDate: Dayjs
 }
-function AttendanceRateChart({ statisticData, tabChange, tab }: IProps) {
-  const labels = ["Red", "Blue", "Yellow"];
-
-  const attendanceTextChange = (timeStatus: PeriodType) => {
-    switch (timeStatus) {
-      case "DAILY":
-        return TODAY_TEXT;
-      case "WEEKLY":
-        return WEEKLY_TEXT;
-      case "MONTHLY":
-        return MONTHLY_TEXT;
-    }
-  };
-
+function AttendanceRateChart({
+  statisticData,
+  tabChange,
+  tab,
+  currentDate,
+}: IProps) {
   return (
     <div className="flex flex-col gap-2">
       <ButtonsWrapper>
         <div className="flex flex-col text-left space-y-[-3px]">
           <p className="text-m-bold">{attendanceTextChange(tab)}</p>
           <p className="text-xs-semibold text-text-secondary">
-            {statisticPeriodText(tab)}
+            {tab !== 'WEEKLY' && statisticPeriodText(currentDate, tab)}
+            {tab === 'WEEKLY' &&
+              statisticData &&
+              statisticData.status === 200 &&
+              statisticData.data.from.replaceAll('-', '.') +
+                '-' +
+                statisticData.data.to.replaceAll('-', '.')}
           </p>
         </div>
         <TimeButtons
@@ -50,19 +46,23 @@ function AttendanceRateChart({ statisticData, tabChange, tab }: IProps) {
         )}
       </ChartWrapper>
     </div>
-  );
+  )
 }
-const ChartWrapper = tw.div`flex flex-col items-center gap-2 w-full h-[284px] bg-white rounded-2xl mx-auto py-4`;
-const ButtonsWrapper = tw.div`flex justify-between w-full items-center`;
-export default React.memo(AttendanceRateChart);
+const ChartWrapper = tw.div`flex flex-col items-center gap-2 w-full h-[284px] bg-white rounded-2xl mx-auto py-4`
+const ButtonsWrapper = tw.div`flex justify-between w-full items-center`
+export default React.memo(AttendanceRateChart)
+
+const TODAY_TEXT = '오늘의 출석률'
+const WEEKLY_TEXT = '이번 주 출석률'
+const MONTHLY_TEXT = '이번 달의 출석률'
 
 const attendanceTextChange = (timeStatus: PeriodType) => {
   switch (timeStatus) {
-    case "DAILY":
-      return TODAY_TEXT;
-    case "WEEKLY":
-      return WEEKLY_TEXT;
-    case "MONTHLY":
-      return MONTHLY_TEXT;
+    case 'DAILY':
+      return TODAY_TEXT
+    case 'WEEKLY':
+      return WEEKLY_TEXT
+    case 'MONTHLY':
+      return MONTHLY_TEXT
   }
-};
+}
