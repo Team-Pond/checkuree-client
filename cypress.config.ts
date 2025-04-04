54256124c2af1940a4f97cf9662a96c9da10e3de
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-04 13:45:39 +0900

    chore: .gitignore에 cypress.env.json 추가 및 Cypress 설정 간소화

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    env: {
-      api_server: 'https://dev.checkuree.com/api/v1',
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 })
