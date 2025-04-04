0b42013128b0d2c2f3c1ab0d914c96f8ac3418dd
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-04 13:17:37 +0900

    feat: Cypress 커맨드 추가 및 타입 정의 업데이트

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,14 @@
+// cypress/support/index.d.ts
+declare namespace Cypress {
+  interface Chainable<Subject = any> {
+    login(): Chainable<any>
+    StudentFormStep1(studentData: {
+      name: string
+      birth: string
+      gender: 'male' | 'female'
+    }): Chainable<any>
+    StudentFormStep2(): Chainable<any>
+    BookFormStep1(bookData: { name: string }): Chainable<any>
+    BookFormStep2(bookData: { name: string }): Chainable<any>
+  }
+}
