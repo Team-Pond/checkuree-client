cbd2beeb8133b8fcff2142f72e13a08373ba1ccc
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-04 13:17:25 +0900

    refactor: 로그인 로직 통합 및 학생/출석부 생성 시나리오 간소화

--- cypress/e2e/attendeeCreate.cy.ts
+++ cypress/e2e/attendeeCreate.cy.ts
@@ -4,55 +4,29 @@ import 'cypress-file-upload'
 describe('학생 등록 페이지', () => {
   // Given
   beforeEach(() => {
-    cy.request('POST', 'https://dev.checkuree.com/api/v1/auth/signin', {
-      username: 'dkandkdlel',
-      password: 'test123123!!',
-    }).then((response) => {
-      const accessToken = response.body.data.accessToken
-      cy.setCookie('accessToken', accessToken)
-    })
-
+    cy.login()
     cy.visit('/book')
   })
 
   it('학생 등록 시나리오', () => {
     // When
 
-    // "출석부" 존재 유무 확인
-    cy.get('[data-cy="book-0"]').should('be.visible')
-
-    // "출석부" 첫번째 클릭
-    cy.get('[data-cy="book-0"]').click()
+    cy.get('[data-cy="book-0"]').should('be.visible').click()
 
     cy.get('[data-cy="navigate-attendee"]').click()
 
     cy.get('[data-cy="icon-attendee-create"]').click()
 
-    // "출석부 이름" 입력 필드가 보이는지 확인
-    cy.get('[data-cy="name-input"]').should('be.visible')
-
-    // "출석부 이름" 입력
-    cy.get('[data-cy="name-input"]').type(Date.now().toString().slice(-5))
-
-    cy.get('[data-cy="birth-input"]').type('19981010')
-
-    cy.get('[data-cy="male-radio"]').click()
-
-    cy.get('[data-cy="today-enroll-label"]').click()
+    cy.StudentFormStep1({
+      name: new Date().toISOString().slice(-5),
+      birth: '20230404',
+      gender: 'male',
+    })
 
     // "다음으로" 버튼 클릭하여 Step2로 이동
     cy.get('[data-cy="stepSubmit"]').click()
 
-    // Step2 화면으로 전환되었는지 확인
-    cy.get('[data-cy="curriculum-select"]').should('be.visible')
-    cy.get('[data-cy="curriculum-select"]').click()
-
-    // 오른쪽 목록에서 과목 아이템의 플러스 아이콘 클릭하여 과목 아이템 추가
-    cy.get('[data-cy="add-icon"]').first().click()
-
-    cy.get('[data-cy="schedule-0"]').first().click()
-
-    cy.get('[data-cy="add-schedule-button"]').click()
+    cy.StudentFormStep2()
 
     cy.get('[data-cy="submit-attendee-create"]').click()
 
