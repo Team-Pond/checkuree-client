f9810c7f323e7066bba509eb5578a3d4fa2c918c
GanziMan <87753974+GanziMan@users.noreply.github.com>
2025-04-04 13:46:56 +0900

    refactor: Cypress test code Refactor & api Server Url env accept (#163)
    * chore: Cypress 관련 파일 및 환경 설정 추가
    
    * refactor: 로그인 로직 통합 및 학생/출석부 생성 시나리오 간소화
    
    * feat: Cypress 커맨드 추가 및 타입 정의 업데이트
    
    * chore: .gitignore에 cypress.env.json 추가 및 Cypress 설정 간소화

--- cypress/e2e/bookCreate.cy.ts
+++ cypress/e2e/bookCreate.cy.ts
@@ -4,66 +4,31 @@ import 'cypress-file-upload'
 describe('출석부 생성 페이지', () => {
   // Given
   beforeEach(() => {
-    cy.request('POST', 'https://dev.checkuree.com/api/v1/auth/signin', {
-      username: 'dkandkdlel',
-      password: 'test123123!!',
-    }).then((response) => {
-      const accessToken = response.body.data.accessToken
-      cy.setCookie('accessToken', accessToken)
-    })
-
+    cy.login()
     cy.visit('/book/create')
   })
 
   it('출석부 생성 시나리오', () => {
     // When
-    // "출석부 이름" 입력 필드가 보이는지 확인
-    cy.get('[data-cy="title"]').should('be.visible')
-
-    // "출석부 이름" 입력
-    cy.get('[data-cy="title"]').type('cypress E2E 테스트')
-
-    // 요일 선택 - 예시로 "월" 버튼 클릭 (Step1에서는 요일 버튼의 텍스트가 "월"로 표시)
-    cy.get('[data-cy="월"]').click()
-
-    // TimePicker 테스트 (여기서는 단순히 버튼 클릭하여 모달 오픈 여부 확인)
-    cy.get('[data-cy="startTime"]').click()
-    cy.get('.fixed.inset-0').click({ force: true })
-    cy.get('.fixed.inset-0').should('not.exist')
-
-    cy.get('[data-cy="endTime"]').click()
-    cy.get('.fixed.inset-0').click({ force: true })
-    cy.get('.fixed.inset-0').should('not.exist')
+    cy.BookFormStep1({
+      name: '출석부 테스트',
+    })
 
     // "다음으로" 버튼 클릭하여 Step2로 이동
     cy.get('[data-cy="stepSubmit"]').click()
 
     // Step2 화면으로 전환되었는지 확인
-    cy.get('[data-cy="curriculum-add-button"]').should('be.visible')
-    cy.get('[data-cy="curriculum-add-button"]').click()
-
-    cy.get('[data-cy="curriculum-input"]')
-      .should('be.visible')
-      .type('커리큘럼 테스트')
-
-    cy.get('[data-cy="subject-add-button"]').click()
-
-    // 오른쪽 목록에서 과목 아이템의 플러스 아이콘 클릭하여 과목 아이템 추가
-    cy.get('[data-cy="add-icon"]').first().click()
-
-    cy.get('[data-cy="bottom-drawer"]').click({ force: true })
-
-    cy.get('[data-cy="curriculum-confirm"]').click()
-
+    cy.BookFormStep2({
+      name: '출석부 커리큘럼 테스트',
+    })
     cy.get('[data-cy="submit-book-create"]').click()
 
     cy.getCookie('accessToken').then((cookie) => {
       const accessToken = cookie?.value
-
       // Then
       cy.request({
         method: 'GET',
-        url: 'https://dev.checkuree.com/api/v1/book/my',
+        url: `${Cypress.env('api_server')}/book/my`,
         headers: { Authorization: `Bearer ${accessToken}` },
       }).then((response) => {
         cy.log('API 응답:', JSON.stringify(response.body.data))
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,54 @@
 //     }
 //   }
 // }
+
+Cypress.Commands.add('login', () => {
+  return cy
+    .request('POST', `${Cypress.env('api_server')}/auth/signin`, {
+      username: 'dkandkdlel',
+      password: 'test123123!!',
+    })
+    .then((response) => {
+      const accessToken = response.body.data.accessToken
+      // cy.setCookie()의 체인 내에서 response를 반환하도록 수정
+      return cy.setCookie('accessToken', accessToken).then(() => {
+        return response
+      })
+    })
+})
+
+Cypress.Commands.add('StudentFormStep1', ({ name, birth, gender }) => {
+  cy.get('[data-cy="name-input"]').should('be.visible').type(name)
+  cy.get('[data-cy="birth-input"]').type(birth)
+  gender === 'male'
+    ? cy.get('[data-cy="male-radio"]').click()
+    : cy.get('[data-cy="female-radio"]').click()
+  cy.get('[data-cy="today-enroll-label"]').click()
+})
+
+Cypress.Commands.add('StudentFormStep2', () => {
+  cy.get('[data-cy="curriculum-select"]').should('be.visible').click()
+  cy.get('[data-cy="add-icon"]').first().click()
+  cy.get('[data-cy="schedule-0"]').first().click()
+  cy.get('[data-cy="add-schedule-button"]').click()
+})
+
+Cypress.Commands.add('BookFormStep1', ({ name }) => {
+  cy.get('[data-cy="title"]').should('be.visible').type(name)
+  cy.get('[data-cy="월"]').click()
+  cy.get('[data-cy="startTime"]').click()
+  cy.get('.fixed.inset-0').click({ force: true })
+  cy.get('.fixed.inset-0').should('not.exist')
+  cy.get('[data-cy="endTime"]').click()
+  cy.get('.fixed.inset-0').click({ force: true })
+  cy.get('.fixed.inset-0').should('not.exist')
+})
+
+Cypress.Commands.add('BookFormStep2', ({ name }) => {
+  cy.get('[data-cy="curriculum-add-button"]').should('be.visible').click()
+  cy.get('[data-cy="curriculum-input"]').should('be.visible').type(name)
+  cy.get('[data-cy="subject-add-button"]').click()
+  cy.get('[data-cy="add-icon"]').first().click()
+  cy.get('[data-cy="bottom-drawer"]').click({ force: true })
+  cy.get('[data-cy="curriculum-confirm"]').click()
+})
