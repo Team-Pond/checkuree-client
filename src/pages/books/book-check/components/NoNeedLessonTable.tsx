e1ea4cf5a748b9870061771c0aa7ecc9022af062
GanziMan <87753974+GanziMan@users.noreply.github.com>
2025-04-06 23:48:59 +0900

    Refactor/20250406 (#165)
    * refactor: Step1 및 Step2 컴포넌트에서 오류 메시지에 data-cy 속성 추가 및 상태 관리 개선
    
    * refactor: 출석부 생성 시나리오 테스트 추가 및 스키마 오류 메시지 개선
    
    * refactor: BookCreate 컴포넌트에서 불필요한 상태 제거 및 유효성 검사 개선
    
    * refactor: Vite 설정 및 NoNeedLessonTable 컴포넌트 코드 스타일 개선
    
    * refactor: NoNeedLessonTable 컴포넌트에서 코드 스타일 개선 및 React.Fragment로 변경

--- src/pages/books/book-check/components/NoNeedLessonTable.tsx
+++ src/pages/books/book-check/components/NoNeedLessonTable.tsx
@@ -1,18 +1,19 @@
-import tw from "tailwind-styled-components";
-import { scheduleCheckformatTime } from "@/utils";
-import LessonRow from "./LessonRow";
-import { ScheduleData, ScheduleDataContentType } from "@/api/type";
+import tw from 'tailwind-styled-components'
+import { scheduleCheckformatTime } from '@/utils'
+import LessonRow from './LessonRow'
+import { ScheduleData, ScheduleDataContentType } from '@/api/type'
+import React from 'react'
 
 interface IProps {
-  noNeedLessonTimeScheduleTable: ScheduleDataContentType;
-  bookId: number;
+  noNeedLessonTimeScheduleTable: ScheduleDataContentType
+  bookId: number
   handleAttendanceStatusWithConfirmation: (
-    targetStatus: "ATTEND" | "ABSENT",
-    schedule: ScheduleData
-  ) => void;
-  currentDate: string;
+    targetStatus: 'ATTEND' | 'ABSENT',
+    schedule: ScheduleData,
+  ) => void
+  currentDate: string
 
-  openModifyRecordTimeModal: (schedule: ScheduleData) => void;
+  openModifyRecordTimeModal: (schedule: ScheduleData) => void
 }
 export default function NoNeedLessonTable(props: IProps) {
   const {
@@ -21,21 +22,21 @@ export default function NoNeedLessonTable(props: IProps) {
     handleAttendanceStatusWithConfirmation,
     openModifyRecordTimeModal,
     currentDate,
-  } = props;
+  } = props
 
   return (
-    <>
+    <React.Fragment>
       {noNeedLessonTimeScheduleTable?.map((content, index) => {
         return (
           content.schedules.length > 0 && (
-            <LessonContainer key={[content.schedules, index].join("-")}>
+            <LessonContainer key={[content.schedules, index].join('-')}>
               <LessonStatusTime>
                 {scheduleCheckformatTime(content.startTime)}
               </LessonStatusTime>
 
               {content.schedules.map((schedule, index) => (
                 <LessonRow
-                  key={[schedule.scheduleId, index].join("-")}
+                  key={[schedule.scheduleId, index].join('-')}
                   schedule={schedule}
                   bookId={bookId}
                   handleAttendanceStatusWithConfirmation={
@@ -47,11 +48,11 @@ export default function NoNeedLessonTable(props: IProps) {
               ))}
             </LessonContainer>
           )
-        );
+        )
       })}
-    </>
-  );
+    </React.Fragment>
+  )
 }
 
-const LessonContainer = tw.div`w-full text-left rounded-2xl bg-white px-6 pt-1 flex flex-col`;
-const LessonStatusTime = tw.p`text-[#5d5d5d] text-s-bold h-12 flex items-center`;
+const LessonContainer = tw.div`w-full text-left rounded-2xl bg-white px-6 pt-1 flex flex-col`
+const LessonStatusTime = tw.p`text-[#5d5d5d] text-s-bold h-12 flex items-center`
