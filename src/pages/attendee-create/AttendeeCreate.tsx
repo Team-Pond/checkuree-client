4551bf5aaaf3e03b6cb460fca3e620ebb9035205
GanziMan <87753974+GanziMan@users.noreply.github.com>
2025-04-14 00:05:11 +0900

    Refactor/use funnel attendee create (#174)
    * Refactor code structure for improved readability and maintainability
    
    * refactor: attendeeCreate refactor form

--- src/pages/attendee-create/AttendeeCreate.tsx
+++ src/pages/attendee-create/AttendeeCreate.tsx
@@ -1,7 +1,5 @@
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
-import { twMerge } from 'tailwind-merge'
 import Step1 from './components/Step1'
-import { useState } from 'react'
 import Step2 from './components/Step2'
 import { getTodayYYYYMMDD } from '@/utils'
 import { FormProvider, useForm } from 'react-hook-form'
@@ -12,13 +10,24 @@ import toast from 'react-hot-toast'
 import SEO from '@/components/SEO'
 import FormHeader from './components/FormHeader'
 import tw from 'tailwind-styled-components'
+import { useFunnel } from '@use-funnel/react-router-dom'
 
+type FunnelContext = {
+  Step1: {}
+  Step2: {}
+}
 export default function AttendeeCreate() {
+  const funnel = useFunnel<FunnelContext>({
+    id: 'attendee-create',
+    initial: {
+      step: 'Step1',
+      context: {},
+    },
+  })
+
   const navigate = useNavigate()
   const location = useLocation()
   const { bookId } = useParams()
-
-  const [isStep2, setIsStep2] = useState<boolean>(false)
 
   const methods = useForm<CreateAttendeeSchema>({
     resolver: zodResolver(AttendeeSchema),
@@ -33,12 +42,6 @@ export default function AttendeeCreate() {
     ])
   }
 
-  const handleStep1Next = async () => {
-    const isValid = await trigger('attendeeRequest')
-    if (isValid) {
-      setIsStep2(true)
-    }
-  }
   const handleStep2Next = async (data: CreateAttendeeSchema) => {
     const isValid = await trigger('attendeeRequest')
     if (isValid) {
@@ -88,51 +91,53 @@ export default function AttendeeCreate() {
         content="체쿠리 음악학원 출석부 서비스의 학생 등록 페이지입니다."
       />
       <Form onSubmit={handleSubmit(handleStep2Next)}>
-        <FormHeader isStep2={isStep2} />
+        <FormHeader isStep2={funnel.step === 'Step2'} />
         <FormInner>
-          {isStep2 ? (
-            <Step2
-              onChangeGrade={onChangeGrade}
-              attendanceBookId={Number(bookId)}
-            />
-          ) : (
-            <Step1 />
-          )}
-          {isStep2 ? (
-            <div className="flex gap-4 w-full">
-              <Button
-                type="button"
-                onClick={() => {
-                  setIsStep2(false)
-                }}
-                data-cy="previous-book-create"
-                aria-label="previous-book-create"
-              >
-                이전으로
-              </Button>
-              <Button
-                type="submit"
-                isSubmit
-                data-cy="submit-attendee-create"
-                aria-label="submit-attendee-create"
-              >
-                생성하기
-              </Button>
-            </div>
-          ) : (
-            <button
-              type="button"
-              data-cy="stepSubmit"
-              aria-label="stepSubmit"
-              className={twMerge(
-                'max-w-[341px] w-full h-[54px] flex justify-center items-center rounded-xl',
-                'bg-bg-tertiary text-[#f1f8f3]',
-              )}
-              onClick={handleStep1Next}
-            >
-              <p className="font-semibold text-lg">다음으로</p>
-            </button>
-          )}
+          <funnel.Render
+            Step1={({ history }) => (
+              <>
+                <Step1 />
+                <Button
+                  issubmit
+                  type="button"
+                  onClick={async () =>
+                    (await trigger('attendeeRequest')) &&
+                    history.push('Step2', {})
+                  }
+                  data-cy="stepSubmit"
+                  aria-label="stepSubmit"
+                >
+                  <p className="font-semibold text-lg">다음으로</p>
+                </Button>
+              </>
+            )}
+            Step2={({ history }) => (
+              <>
+                <Step2
+                  onChangeGrade={onChangeGrade}
+                  attendanceBookId={Number(bookId)}
+                />
+                <div className="flex gap-4 w-full">
+                  <Button
+                    type="button"
+                    onClick={() => history.back()}
+                    data-cy="previous-book-create"
+                    aria-label="previous-book-create"
+                  >
+                    이전으로
+                  </Button>
+                  <Button
+                    type="submit"
+                    issubmit
+                    data-cy="submit-attendee-create"
+                    aria-label="submit-attendee-create"
+                  >
+                    생성하기
+                  </Button>
+                </div>
+              </>
+            )}
+          />
         </FormInner>
       </Form>
     </FormProvider>
@@ -141,4 +146,4 @@ export default function AttendeeCreate() {
 
 const Form = tw.form`flex flex-col gap-10 w-full pb-[30px]`
 const FormInner = tw.div`flex flex-col w-full mx-auto justify-center items-center gap-6 max-w-[342px]`
-const Button = tw.button`${({ isSubmit }: { isSubmit?: boolean }) => (isSubmit ? 'bg-bg-tertiary text-[#F1F8F3]' : 'bg-bg-secondary text-text-secondary')} text-lg font-semibold w-full h-[54px] flex justify-center items-center rounded-2xl `
+const Button = tw.button`${({ issubmit }: { issubmit?: boolean }) => (issubmit ? 'bg-bg-tertiary text-[#F1F8F3]' : 'bg-bg-secondary text-text-secondary')} text-lg font-semibold w-full h-[54px] flex justify-center items-center rounded-2xl `
