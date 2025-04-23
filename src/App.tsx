e21209714686e3bbae77fb87b1fc58b827707fa7
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-23 14:45:51 +0900

    Merge branch 'develop' of https://github.com/Team-Pond/pond-client into test/test-commit-PR

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const routes: RouteType[] = [
   },
   {
     path: '/book/:bookId/attendee/:attendeeId/schedule',
-    element: <ScheduleModify />, //
+    element: <ScheduleModify />, // 학생 일정 수정
   },
   {
     path: '/book/:bookId/attendee/:attendeeId/counselling',
@@ -77,7 +77,6 @@ function App() {
           <Route path="/checkuree-auth/signin" element={<CheckureeSignIn />} />
           <Route path="/auth/login" element={<KakaoSignIn />} />
           {/* 인증 처리 */}
-          {/* Book 관련 라우트 그룹 */}
           <Route element={<BookLayout />}>
             {routes.map((route) => (
               <Route
