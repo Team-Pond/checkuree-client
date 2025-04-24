ff011cfce287edc69feeb8463995ac8632801ba1
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-24 18:53:53 +0900

    refactor: attendee 및 book 관련 API 호출 개선 및 쿼리 함수 추가

--- src/queryKeys.ts
+++ src/queryKeys.ts
@@ -6,7 +6,23 @@ import {
   GenderType,
   PeriodType,
 } from './api/type'
-import { getScheduleAttendee } from './api/ScheduleApiClient'
+import { getSchedule } from './api/ScheduleApiClient'
+import {
+  getBookCourse,
+  getBookDetail,
+  getBookScheduleTable,
+  getMeBooks,
+} from './api/AttendanceBookApiClient'
+import {
+  getAttendee,
+  getAttendeeDetail,
+  getAttendeeProgressLog,
+  getAttendeeStatistics,
+  getScheduleAttendee,
+} from './api/AttendeeApiClient'
+import { getStatistics } from './api/RecordApiClient'
+import { getSubjectItems, getSubjects } from './api/CourseApiClient'
+import { getAttendeeCounsellings } from './api/CounselApiClient'
 
 // 수강생 데이터를 위한 키
 export const attendeeKeys = createQueryKeys('attendee', {
@@ -17,18 +33,66 @@ export const attendeeKeys = createQueryKeys('attendee', {
     age: { min: number; max: number },
   ) => ({
     queryKey: [bookId, dayArrays, gender, age],
+    queryFn: async () => {
+      const response = await getAttendee({
+        attendanceBookId: Number(bookId),
+        filter: {
+          age: age,
+          gradeIds: [0],
+          scheduleDays: dayArrays,
+          gender: gender,
+          status: 'ATTENDING',
+        },
+      })
+      if (response.status === 200) return response
+    },
   }),
-  detail: (attendeeId: number) => ({
-    queryKey: [attendeeId],
+  detail: (bookId: number, attendeeId: number) => ({
+    queryKey: [bookId, attendeeId],
+    queryFn: async () =>
+      await getAttendeeDetail({
+        attendanceBookId: bookId,
+        attendeeId: attendeeId,
+      }).then((res) => res.data),
   }),
+
+  // 스케줄 시간표 학생 데이터
   schedules: (attendanceBookId: number, dayOfWeek?: string, hhmm?: string) => ({
     queryKey: [attendanceBookId, dayOfWeek, hhmm],
+    queryFn: async () => {
+      const res = await getScheduleAttendee({
+        attendanceBookId,
+        dayOfWeek: dayOfWeek!,
+        hhmm: hhmm!,
+      })
+      if (res.status === 200) return res.data
+    },
   }),
+
+  // 수강생 스케줄 데이터
+  scheduleTable: (attendanceBookId: number) => ({
+    queryKey: [attendanceBookId],
+    queryFn: async () => {
+      const res = await getBookScheduleTable({ attendanceBookId })
+      if (res.status === 200) return res.data
+      throw new Error('Failed to fetch schedule table')
+    },
+  }),
+
   courses: (bookId: number) => ({
     queryKey: [bookId],
+    queryFn: async () => {
+      const res = await getBookCourse(String(bookId))
+      if (res.status === 200) return res.data
+    },
   }),
   progressLog: (bookId: number, attendeeId: number) => ({
     queryKey: [bookId, attendeeId],
+    queryFn: async () =>
+      await getAttendeeProgressLog({
+        attendanceBookId: Number(bookId),
+        attendeeId: Number(attendeeId),
+      }).then((res) => res.data),
   }),
 })
 
@@ -36,7 +100,7 @@ export const bookKeys = createQueryKeys('book', {
   schedules: (bookId?: number, formattedDate?: string) => ({
     queryKey: [bookId, formattedDate],
     queryFn: async () =>
-      await getScheduleAttendee({
+      await getSchedule({
         attendanceBookId: Number(bookId!),
         params: {
           date: formattedDate!,
@@ -50,26 +114,76 @@ export const bookKeys = createQueryKeys('book', {
   }),
   list: () => ({
     queryKey: [''],
+    queryFn: async () => {
+      const response = await getMeBooks()
+      if (response.status === 200) {
+        return response
+      }
+    },
   }),
   detail: (bookId: number) => ({
     queryKey: [bookId],
+    queryFn: async () => {
+      const response = await getBookDetail(bookId)
+      if (response.status === 200) {
+        return response
+      }
+    },
   }),
   subjects: () => ({
     queryKey: [''],
+    queryFn: async () =>
+      await getSubjects().then((res) => {
+        if (res.status === 200) return res.data
+      }),
   }),
   subjectCourses: (subjectId: number) => ({
     queryKey: [subjectId],
+    queryFn: async () =>
+      await getSubjectItems({ subjectId: String(subjectId) }).then((res) => {
+        if (res.status === 200) {
+          return res.data
+        }
+      }),
   }),
   statistic: (bookId: number, from: string, periodType: PeriodType) => ({
     queryKey: [bookId, from, periodType],
+    queryFn: async () => {
+      const response = await getStatistics({
+        params: {
+          from,
+          periodType,
+        },
+        attendanceBookId: bookId,
+      })
+      if (response.status === 200) {
+        return response
+      }
+    },
   }),
   statisticAttendee: (bookId: number, type: AttendeeStatisticsType) => ({
     queryKey: [bookId, type],
+    queryFn: async () => {
+      const response = await getAttendeeStatistics({
+        params: {
+          type: type,
+        },
+        attendanceBookId: bookId,
+      })
+      if (response.status === 200) {
+        return response
+      }
+    },
   }),
 })
 
 export const counsellingKeys = createQueryKeys('counselling', {
-  list: (attendeeId: number) => ({
-    queryKey: [attendeeId],
+  list: (bookId: number, attendeeId: number) => ({
+    queryKey: [bookId, attendeeId],
+    queryFn: async () =>
+      await getAttendeeCounsellings({
+        bookId,
+        attendeeId,
+      }).then((res) => res.data),
   }),
 })
