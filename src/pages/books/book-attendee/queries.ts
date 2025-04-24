9388c464a7c12ec1ee255a2b1e411f210ea8eaff
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-04-24 18:54:27 +0900

    refactor: useAttendeeList 훅에서 불필요한 API 호출 제거 및 코드 간소화

--- src/pages/books/book-attendee/queries.ts
+++ src/pages/books/book-attendee/queries.ts
@@ -1,7 +1,6 @@
-import { getAttendee } from "@/api/AttendeeApiClient";
-import { DaysType, GenderType } from "@/api/type";
-import { attendeeKeys } from "@/queryKeys";
-import { useQuery } from "@tanstack/react-query";
+import { DaysType, GenderType } from '@/api/type'
+import { attendeeKeys } from '@/queryKeys'
+import { useQuery } from '@tanstack/react-query'
 
 export const useAttendeeList = ({
   bookId,
@@ -9,25 +8,8 @@ export const useAttendeeList = ({
   gender,
   age,
 }: {
-  bookId: number;
-  dayArrays: DaysType[];
-  gender: GenderType;
-  age: { min: number; max: number };
-}) => {
-  return useQuery({
-    queryKey: attendeeKeys.list(bookId, dayArrays, gender, age).queryKey,
-    queryFn: async () => {
-      const response = await getAttendee({
-        attendanceBookId: Number(bookId),
-        filter: {
-          age: age,
-          gradeIds: [0],
-          scheduleDays: dayArrays,
-          gender: gender,
-          status: "ATTENDING",
-        },
-      });
-      if (response.status === 200) return response;
-    },
-  });
-};
+  bookId: number
+  dayArrays: DaysType[]
+  gender: GenderType
+  age: { min: number; max: number }
+}) => useQuery(attendeeKeys.list(bookId, dayArrays, gender, age))
