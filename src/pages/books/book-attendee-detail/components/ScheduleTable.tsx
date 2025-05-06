bc169f65e5211f3ced1d5f52e46e7c74ede0bc7d
Sang Hoo Park <117077999+Hoo93@users.noreply.github.com>
2025-05-07 00:37:39 +0900

    Feat/ 학생 스케쥴 page 미래 스케쥴 표시 추가 (#183)
    * feat: 선택된 미래 데이터는 빨간 테두리로 표현 되는 기능 추가
    
    * feat: 스케쥴 표시 기능 추가

--- src/pages/books/book-attendee-detail/components/ScheduleTable.tsx
+++ src/pages/books/book-attendee-detail/components/ScheduleTable.tsx
@@ -1,31 +1,71 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useBookDetail } from "../../queries";
-import { UpdateAttendeeScheduleRequest } from "@/api/AttendeeSchema";
-import { dayMap, DayOfWeek } from "@/api/type";
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useBookDetail } from '../../queries'
+import { UpdateAttendeeScheduleRequest } from '@/api/AttendeeSchema'
+import { dayMap, DayOfWeek } from '@/api/type'
 
 interface ScheduleItem {
-  dayOfWeek: DayOfWeek;
-  scheduleCount: number[];
+  dayOfWeek: DayOfWeek
+  scheduleCount: number[]
 }
 
 interface ScheduleProps {
-  scheduleTable: ScheduleItem[];
-  timeSlots: number;
-  startHhmm: string;
-  endHhmm: string;
-  handleSchedule: (
-    dayOfWeek: string,
-    hhmm: string,
-    isSelected: boolean
-  ) => void;
-  handleAttendeeBottomDrawer: (state: boolean) => void;
-  attendeeSchedules?: UpdateAttendeeScheduleRequest;
+  scheduleTable: ScheduleItem[]
+  timeSlots: number
+  startHhmm: string
+  endHhmm: string
+  handleSchedule: (dayOfWeek: string, hhmm: string, isSelected: boolean) => void
+  handleAttendeeBottomDrawer: (state: boolean) => void
+  attendeeSchedules?: UpdateAttendeeScheduleRequest
+  futureSchedules?: UpdateAttendeeScheduleRequest
 }
 
 function parseHhmm(hhmm: string) {
-  const [hh, mm] = hhmm.split(":").map(Number);
-  return { hour: hh, minute: mm };
+  const [hh, mm] = hhmm.split(':').map(Number)
+  return { hour: hh, minute: mm }
+}
+
+function getBeforeHhmm(hhmm: string) {
+  const [hh, mm] = hhmm.split(':')
+
+  if (mm === '30') return `${hh}:00`
+
+  const hour = parseInt(hh)
+
+  return hour >= 1 ? `${String(hour - 1).padStart(2, '0')}:30` : '00:00'
+}
+
+function getBorderClass(
+  futureSchedules: { hhmm: string; day: string }[] | undefined,
+  day: DayOfWeek,
+  hhmm: string,
+  beforeHhmm: string,
+) {
+  if (!futureSchedules) return ''
+
+  for (const schedule of futureSchedules) {
+    if (schedule.day !== day) continue
+
+    if (schedule.hhmm === hhmm) {
+      return [
+        'border-r-red-400 border-r-[2px]',
+        'border-l-red-400 border-l-[2px]',
+        'border-b-[#f6f6f6]',
+        'border-t-red-400 border-t-[2px]',
+      ].join(' ')
+    }
+
+    if (schedule.hhmm === beforeHhmm) {
+      return [
+        'border-r-red-400 border-r-[2px]',
+        'border-l-red-400 border-l-[2px]',
+        'border-t-[#f6f6f6]',
+        'border-b-red-400 border-b-[2px]',
+      ].join(' ')
+    }
+  }
+
+  return ''
 }
 
 const ScheduleTable: React.FC<ScheduleProps> = ({
@@ -34,49 +74,59 @@ const ScheduleTable: React.FC<ScheduleProps> = ({
   endHhmm,
   handleSchedule,
   attendeeSchedules,
+  // FIXME: Dummy Data
+  futureSchedules = {
+    schedules: [
+      { hhmm: '13:00', day: 'MONDAY' },
+      { hhmm: '13:30', day: 'TUESDAY' },
+      { hhmm: '14:00', day: 'WEDNESDAY' },
+      { hhmm: '14:30', day: 'THURSDAY' },
+      { hhmm: '15:00', day: 'FRIDAY' },
+    ],
+  },
 }) => {
-  const { bookId } = useParams();
-  const { data: bookDetail } = useBookDetail(Number(bookId));
+  const { bookId } = useParams()
+  const { data: bookDetail } = useBookDetail(Number(bookId))
 
-  const start = parseHhmm(startHhmm);
-  const end = parseHhmm(endHhmm);
-  const totalHours = end.hour - start.hour;
+  const start = parseHhmm(startHhmm)
+  const end = parseHhmm(endHhmm)
+  const totalHours = end.hour - start.hour
   const hoursArray = Array.from(
     { length: totalHours },
-    (_, i) => start.hour + i
-  );
+    (_, i) => start.hour + i,
+  )
 
   const availableDaysSet = new Set([
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-  ]);
+    'MONDAY',
+    'TUESDAY',
+    'WEDNESDAY',
+    'THURSDAY',
+    'FRIDAY',
+  ])
 
   const [filteredScheduleTable, setFilteredScheduleTable] =
-    useState(scheduleTable);
+    useState(scheduleTable)
 
   useEffect(() => {
     if (bookDetail?.data?.availableDays) {
-      bookDetail.data.availableDays.forEach((day) => availableDaysSet.add(day));
+      bookDetail.data.availableDays.forEach((day) => availableDaysSet.add(day))
 
       const weekendFiltered = scheduleTable.filter((daySchedule) =>
-        availableDaysSet.has(daySchedule.dayOfWeek)
-      );
+        availableDaysSet.has(daySchedule.dayOfWeek),
+      )
 
-      const isAvailableFrom30 = bookDetail?.data?.availableFrom?.endsWith("30");
+      const isAvailableFrom30 = bookDetail?.data?.availableFrom?.endsWith('30')
 
       const updatedScheduleTable = isAvailableFrom30
         ? weekendFiltered.map((daySchedule) => ({
             ...daySchedule,
             scheduleCount: [0, ...daySchedule.scheduleCount, 0], // 30분 간격 보정
           }))
-        : weekendFiltered;
+        : weekendFiltered
 
-      setFilteredScheduleTable(updatedScheduleTable);
+      setFilteredScheduleTable(updatedScheduleTable)
     }
-  }, [bookDetail, scheduleTable]);
+  }, [bookDetail, scheduleTable])
 
   return (
     <div className="max-w-4xl mx-auto">
@@ -96,8 +146,8 @@ const ScheduleTable: React.FC<ScheduleProps> = ({
         </thead>
         <tbody>
           {hoursArray.map((hour, hourIdx) => {
-            const slotIndex = hourIdx * 2;
-            const secondSlotIndex = slotIndex + 1;
+            const slotIndex = hourIdx * 2
+            const secondSlotIndex = slotIndex + 1
 
             const firstRow = (
               <tr key={`${hour}-first`}>
@@ -108,29 +158,35 @@ const ScheduleTable: React.FC<ScheduleProps> = ({
                   {hour}
                 </td>
                 {filteredScheduleTable.map((dayData) => {
-                  const count = dayData.scheduleCount[slotIndex];
-                  const hhmm = `${hour}:00`;
-                  const beforeHhmm =
-                    hour >= 1
-                      ? `${String(hour - 1).padStart(2, "0")}:30`
-                      : "00:00";
+                  const count = dayData.scheduleCount[slotIndex]
+                  const hhmm = `${hour}:00`
+                  const beforeHhmm = getBeforeHhmm(hhmm)
 
                   const isSelected =
                     attendeeSchedules?.schedules.some(
                       (schedule) =>
                         schedule.day === dayData.dayOfWeek &&
-                        (schedule.hhmm === hhmm || schedule.hhmm === beforeHhmm)
-                    ) ?? false;
+                        (schedule.hhmm === hhmm ||
+                          schedule.hhmm === beforeHhmm),
+                    ) ?? false
+
+                  const borderClass = getBorderClass(
+                    futureSchedules?.schedules,
+                    dayData.dayOfWeek,
+                    hhmm,
+                    beforeHhmm,
+                  )
 
                   return (
                     <td
                       key={`${dayData.dayOfWeek}-${slotIndex}`}
-                      className={`border border-[#f6f6f6] text-sm w-[54px] h-[34px] align-middle cursor-pointer ${
+                      className={`border border-[#f6f6f6] ${borderClass}
+                      text-sm w-[54px] h-[34px] align-middle cursor-pointer ${
                         isSelected
-                          ? "bg-bg-tertiary text-text-interactive-inverse text-xs-medium"
+                          ? 'bg-bg-tertiary text-text-interactive-inverse text-xs-medium'
                           : count > 0
-                          ? "bg-bg-primary text-text-secondary text-xs-medium"
-                          : "bg-bg-secondary text-text-secondary text-xs-medium"
+                            ? 'bg-bg-primary text-text-secondary text-xs-medium'
+                            : 'bg-bg-secondary text-text-secondary text-xs-medium'
                       }`}
                       onClick={() =>
                         handleSchedule(dayData.dayOfWeek, hhmm, isSelected)
@@ -138,34 +194,43 @@ const ScheduleTable: React.FC<ScheduleProps> = ({
                     >
                       {count}명
                     </td>
-                  );
+                  )
                 })}
               </tr>
-            );
+            )
 
             const secondRow = (
               <tr key={`${hour}-second`}>
                 {filteredScheduleTable.map((dayData) => {
-                  const count = dayData.scheduleCount[secondSlotIndex];
-                  const hhmm = `${hour}:30`;
-                  const beforeHhmm = `${hour}:00`;
+                  const count = dayData.scheduleCount[secondSlotIndex]
+                  const hhmm = `${hour}:30`
+                  const beforeHhmm = `${hour}:00`
 
                   const isSelected =
                     attendeeSchedules?.schedules.some(
                       (schedule) =>
                         schedule.day === dayData.dayOfWeek &&
-                        (schedule.hhmm === hhmm || schedule.hhmm === beforeHhmm)
-                    ) ?? false;
+                        (schedule.hhmm === hhmm ||
+                          schedule.hhmm === beforeHhmm),
+                    ) ?? false
+
+                  const borderClass = getBorderClass(
+                    futureSchedules.schedules,
+                    dayData.dayOfWeek,
+                    hhmm,
+                    beforeHhmm,
+                  )
 
                   return (
                     <td
                       key={`${dayData.dayOfWeek}-${secondSlotIndex}`}
-                      className={`border border-[#f6f6f6] text-sm w-[54px] h-[34px] align-middle cursor-pointer ${
+                      className={`border border-[#f6f6f6] ${borderClass}
+                      text-sm w-[54px] h-[34px] align-middle cursor-pointer ${
                         isSelected
-                          ? "bg-bg-tertiary text-text-interactive-inverse text-xs-medium"
+                          ? 'bg-bg-tertiary text-text-interactive-inverse text-xs-medium'
                           : count > 0
-                          ? "bg-bg-primary text-text-secondary text-xs-medium"
-                          : "bg-bg-secondary text-text-secondary text-xs-medium"
+                            ? 'bg-bg-primary text-text-secondary text-xs-medium'
+                            : 'bg-bg-secondary text-text-secondary text-xs-medium'
                       }`}
                       onClick={() =>
                         handleSchedule(dayData.dayOfWeek, hhmm, isSelected)
@@ -173,22 +238,22 @@ const ScheduleTable: React.FC<ScheduleProps> = ({
                     >
                       {count}명
                     </td>
-                  );
+                  )
                 })}
               </tr>
-            );
+            )
 
             return (
               <React.Fragment key={hour}>
                 {firstRow}
                 {secondRow}
               </React.Fragment>
-            );
+            )
           })}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default ScheduleTable;
+export default ScheduleTable
