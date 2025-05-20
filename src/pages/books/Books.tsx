d749118c3c09ab541f13ed7509e41d3b8a6ba844
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-05-20 16:25:13 +0900

    refactor: BookContext 및 관련 컴포넌트에서 Book 인터페이스를 BookType으로 변경

--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,26 +1,26 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { Book, BookContext } from "@/context/BookContext";
-import { useBookList } from "./queries";
-import SEO from "@/components/SEO";
-import MainContent from "./components/MainContent";
-import Header from "./components/Header";
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { BookType, BookContext } from '@/context/BookContext'
+import { useBookList } from './queries'
+import SEO from '@/components/SEO'
+import MainContent from './components/MainContent'
+import Header from './components/Header'
 
 export default function Books() {
-  const navigate = useNavigate();
-  const context = useContext(BookContext);
+  const navigate = useNavigate()
+  const context = useContext(BookContext)
 
-  const { setSelectedBook } = context!;
+  const { setSelectedBook } = context!
 
-  const handleNavigation = (bookData: Book) => {
-    setSelectedBook(bookData);
-    navigate(`/book/${bookData.id}?bookName=${bookData.title}`);
-  };
+  const handleNavigation = (bookData: BookType) => {
+    setSelectedBook(bookData)
+    navigate(`/book/${bookData.id}?bookName=${bookData.title}`)
+  }
 
-  const { data: bookList } = useBookList();
+  const { data: bookList } = useBookList()
 
   return (
-    <section className="relative flex flex-col w-full min-h-screen">
+    <section className="relative flex flex-col w-full min-h-screen ">
       <SEO
         title="체쿠리 | 출석부 목록"
         content="체쿠리 음악학원 출석부 서비스의 출석부 목록 페이지입니다."
@@ -35,7 +35,7 @@ export default function Books() {
 
       <div className="fixed bottom-11 right-[5%] max-w-[390px]">
         <button
-          onClick={() => navigate("/book/create")}
+          onClick={() => navigate('/book/create')}
           className="w-[104px] h-[46px] rounded-full flex gap-2 justify-center items-center bg-bg-tertiary"
         >
           <img
@@ -48,5 +48,5 @@ export default function Books() {
         </button>
       </div>
     </section>
-  );
+  )
 }
--- src/pages/books/components/MainContent.tsx
+++ src/pages/books/components/MainContent.tsx
@@ -1,61 +1,80 @@
-import { Book } from '@/context/BookContext'
+import { BookType } from '@/context/BookContext'
 import { formatTimeRange, getDayGroupFromInput } from '@/utils'
 
 interface MainContentProps {
-  bookList: Book[]
-  handleNavigation: (bookData: Book) => void
+  bookList: BookType[]
+  handleNavigation: (bookData: BookType) => void
 }
 export default function MainContent({
   bookList,
   handleNavigation,
 }: MainContentProps) {
   return (
-    <div className="flex-1 bg-bg-secondary flex flex-col items-center">
-      <div className="w-full max-w-[340px]  border-spacing-0 py-5 grid grid-cols-2  gap-y-6 gap-x-4 ">
+    <div className=" bg-bg-secondary w-full h-full">
+      <div className="w-full py-5 grid grid-cols-2 gap-y-6 gap-x-4 justify-items-stretch mx-auto">
         {bookList.map((attendance, index) => {
           return (
-            <div
+            <Book
               key={attendance.id}
-              data-cy={`book-${index}`}
-              aria-label={`book-${index}`}
-              className="max-w-[162px] w-full"
-              onClick={() => {
-                handleNavigation({
-                  id: attendance.id,
-                  title: attendance.title,
-                  attendeeCount: attendance.attendeeCount,
-                  availableDays: attendance.availableDays,
-                  availableFrom: attendance.availableFrom,
-                  availableTo: attendance.availableTo,
-                })
-              }}
-            >
-              <img
-                src={'/images/img-test.png'}
-                className="w-full h-[97px] rounded-t-2xl"
-                alt=""
-              />
-              <div className="flex flex-col gap-2 px-3 py-4 text-left rounded-b-2xl h-[98px] bg-white">
-                <div className="flex gap-2">
-                  <p className="font-bold text-text-primary max-w-[119px] truncate">
-                    {attendance.title}
-                  </p>
-                  <p className="font-semibold text-text-secondary">
-                    {attendance.attendeeCount}
-                  </p>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <p className="text-text-brand font-bold text-sm">
-                    {getDayGroupFromInput(attendance.availableDays)}
-                  </p>
-                  <p className="text-text-secondary font-medium text-sm">
-                    {formatTimeRange('1200', '2000')}
-                  </p>
-                </div>
-              </div>
-            </div>
+              attendance={attendance}
+              index={index}
+              handleNavigation={handleNavigation}
+            />
           )
         })}
+      </div>
+    </div>
+  )
+}
+
+function Book({
+  attendance,
+  index,
+  handleNavigation,
+}: {
+  attendance: BookType
+  index: number
+  handleNavigation: (bookData: BookType) => void
+}) {
+  return (
+    <div
+      key={attendance.id}
+      data-cy={`book-${index}`}
+      aria-label={`book-${index}`}
+      className={`max-w-[162px] w-full h-[195px] ${index % 2 === 1 ? 'justify-self-start' : 'justify-self-end'}`}
+      onClick={() => {
+        handleNavigation({
+          id: attendance.id,
+          title: attendance.title,
+          attendeeCount: attendance.attendeeCount,
+          availableDays: attendance.availableDays,
+          availableFrom: attendance.availableFrom,
+          availableTo: attendance.availableTo,
+        })
+      }}
+    >
+      <img
+        src={'/images/img-test.png'}
+        className="w-full h-[97px] rounded-t-2xl"
+        alt=""
+      />
+      <div className="flex flex-col gap-2 px-3 py-4 text-left rounded-b-2xl h-[98px] bg-white">
+        <div className="flex gap-2">
+          <p className="font-bold leading-5 text-text-primary max-w-[119px] truncate">
+            {attendance.title}
+          </p>
+          <p className="font-semibold leading-5 text-text-secondary">
+            {attendance.attendeeCount}
+          </p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="text-text-brand font-bold text-sm leading-4">
+            {getDayGroupFromInput(attendance.availableDays)}
+          </p>
+          <p className="text-text-secondary font-medium text-sm leading-4">
+            {formatTimeRange('1200', '2000')}
+          </p>
+        </div>
       </div>
     </div>
   )
