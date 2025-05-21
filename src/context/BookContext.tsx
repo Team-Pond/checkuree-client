6288733aef93924ee5552a51580af9bc9ba370fe
GanziMan <87753974+GanziMan@users.noreply.github.com>
2025-05-21 18:57:04 +0900

    refactor: BookContext 및 관련 컴포넌트에서 Book 인터페이스를 BookType으로 변경 (#187)

--- src/context/BookContext.tsx
+++ src/context/BookContext.tsx
@@ -1,31 +1,31 @@
 // src/context/BookContext.tsx
-import React, { createContext, useState, ReactNode } from "react";
-import { DayOfWeek } from "../utils";
+import React, { createContext, useState, ReactNode } from 'react'
+import { DayOfWeek } from '../utils'
 
-export interface Book {
-  title: string;
-  id: number;
-  attendeeCount: number;
-  availableDays: DayOfWeek[];
-  availableFrom: string;
-  availableTo: string;
+export interface BookType {
+  title: string
+  id: number
+  attendeeCount: number
+  availableDays: DayOfWeek[]
+  availableFrom: string
+  availableTo: string
 }
 
 interface BookContextType {
-  selectedBook: Book | null;
-  setSelectedBook: (book: Book) => void;
+  selectedBook: BookType | null
+  setSelectedBook: (book: BookType) => void
 }
 
-const BookContext = createContext<BookContextType | undefined>(undefined);
+const BookContext = createContext<BookContextType | undefined>(undefined)
 
 const BookProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [selectedBook, setSelectedBook] = useState<BookType | null>(null)
 
   return (
     <BookContext.Provider value={{ selectedBook, setSelectedBook }}>
       {children}
     </BookContext.Provider>
-  );
-};
+  )
+}
 
-export { BookProvider, BookContext };
+export { BookProvider, BookContext }
