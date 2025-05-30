33865be4832b8762afac277e8a0e8547dc6b4c65
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-05-30 13:17:08 +0900

    fix: MainContent 컴포넌트에서 attendance를 book으로 변경하고 관련 로직 수정 docs: 유틸리티 함수에 대한 주석 추가 및 설명 개선

--- src/pages/book/_components/MainContent.tsx
+++ src/pages/book/_components/MainContent.tsx
@@ -12,11 +12,11 @@ export default function MainContent({
   return (
     <div className=" bg-bg-secondary w-full h-full">
       <div className="w-full py-5 grid grid-cols-2 gap-y-6 gap-x-4 justify-items-stretch mx-auto">
-        {bookList.map((attendance, index) => {
+        {bookList.map((book, index) => {
           return (
             <Book
-              key={attendance.id}
-              attendance={attendance}
+              key={book.id}
+              book={book}
               index={index}
               handleNavigation={handleNavigation}
             />
@@ -28,29 +28,30 @@ export default function MainContent({
 }
 
 function Book({
-  attendance,
+  book,
   index,
   handleNavigation,
 }: {
-  attendance: BookType
+  book: BookType
   index: number
   handleNavigation: (bookData: BookType) => void
 }) {
+  const {
+    id,
+    title,
+    attendeeCount,
+    availableDays,
+    availableFrom,
+    availableTo,
+  } = book
   return (
     <div
-      key={attendance.id}
+      key={id}
       data-cy={`book-${index}`}
       aria-label={`book-${index}`}
       className={`max-w-[162px] cursor-pointer w-full h-[195px] ${index % 2 === 1 ? 'justify-self-start' : 'justify-self-end'}`}
       onClick={() => {
-        handleNavigation({
-          id: attendance.id,
-          title: attendance.title,
-          attendeeCount: attendance.attendeeCount,
-          availableDays: attendance.availableDays,
-          availableFrom: attendance.availableFrom,
-          availableTo: attendance.availableTo,
-        })
+        handleNavigation(book)
       }}
     >
       <img
@@ -61,18 +62,22 @@ function Book({
       <div className="flex flex-col gap-2 px-3 py-4 text-left rounded-b-2xl h-[98px] bg-white">
         <div className="flex gap-2">
           <p className="font-bold leading-5 text-text-primary max-w-[119px] truncate">
-            {attendance.title}
+            {title}
           </p>
           <p className="font-semibold leading-5 text-text-secondary">
-            {attendance.attendeeCount}
+            {attendeeCount}
           </p>
         </div>
         <div className="flex flex-col gap-1">
           <p className="text-text-brand font-bold text-sm leading-4">
-            {getDayGroupFromInput(attendance.availableDays)}
+            {getDayGroupFromInput(availableDays)}
           </p>
-          <p className="text-text-secondary font-medium text-sm leading-4">
-            {formatTimeRange('1200', '2000')}
+          <p className="text-text-secondary font-medium text-[13px] leading-4">
+            {formatTimeRange(
+              availableFrom.replace(':', ''),
+              availableTo.replace(':', ''),
+            )}
+            {/* {formatTimeRange('0800', '2100')} */}
           </p>
         </div>
       </div>
