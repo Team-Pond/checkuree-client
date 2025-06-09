62ed16cfb227c0224eb55189ae8bc004cc336e47
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-06-09 23:58:56 +0900

    fix: AttendeeDrawer에서 중복 데이터 처리 및 키 변경

--- src/components/AttendeeDrawer.tsx
+++ src/components/AttendeeDrawer.tsx
@@ -34,14 +34,13 @@ export default function AttendeeDrawer({
   const { dayOfWeek, hhmm, isSelected } = scheduleParams
 
   return (
-    <BottomDrawer isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col gap-4 items-center h-[249px] overflow-auto">
-        {/* Drawer 상단 영역 */}
-        <div className="text-left w-full h-10 border-b border-[#f6f6f6] flex items-center text-s-semibold text-text-secondary">
-          ({convertEngDayToKorDay(dayOfWeek)}) {hhmm}-{' '}
-          {getAddMinuteHhmm(hhmm, 30)}
-        </div>
-
+    <BottomDrawer isOpen={isOpen} onClose={onClose} className="py-[11px] gap-5">
+      {/* Drawer 상단 영역 */}
+      <div className="text-left w-full h-10 border-b border-[#f6f6f6] flex items-center text-s-semibold text-text-secondary">
+        ({convertEngDayToKorDay(dayOfWeek)}) {hhmm}-{' '}
+        {getAddMinuteHhmm(hhmm, 30)}
+      </div>
+      <div className="flex flex-col gap-4 items-center  overflow-auto">
         {/* 수강생 목록 */}
 
         <div className="grid grid-cols-2 max-h-[130px] overflow-y-auto w-full px-[6px]">
@@ -56,29 +55,193 @@ export default function AttendeeDrawer({
               </span>
             </div>
           ))}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}{' '}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}
+          {scheduleData?.map((attendee, index) => (
+            <div
+              // 중복 데이터가 발생하는 현상이 있어 key 변경
+              key={[attendee.name, attendee.age, index].join('-')}
+              className="h-9 w-full text-left"
+            >
+              <span className="text-m-bold text-text-primary">
+                {attendee.name}
+              </span>
+            </div>
+          ))}
         </div>
-
-        <div className="flex gap-4 w-full bottom-5 ">
-          <Button
-            label="닫기"
-            data-cy="close-schedule-button"
-            aria-label="close-schedule-button"
-            onClick={() => onClose()}
-            className="w-full h-[54px] flex justify-center items-center rounded-2xl bg-bg-secondary text-text-secondary text-l-semibold"
-          />
-          <Button
-            label={isSelected ? '삭제하기' : '추가하기'}
-            data-cy="add-schedule-button"
-            aria-label="add-schedule-button"
-            className="w-full h-[54px] flex justify-center items-center rounded-2xl bg-bg-tertiary text-[#F1F8F3] text-l-semibold"
-            onClick={() => {
-              !isSelected
-                ? handleAttendeeSchedules(dayOfWeek as DaysType, hhmm)
-                : handleRemoveAttendeeSchedules(dayOfWeek as DaysType, hhmm),
-                onClose()
-            }}
-          />
-        </div>
+      </div>
+      <div className="flex gap-4 w-full">
+        <Button
+          label="닫기"
+          data-cy="close-schedule-button"
+          aria-label="close-schedule-button"
+          onClick={() => onClose()}
+          className="w-full h-[54px] flex justify-center items-center rounded-2xl bg-bg-secondary text-text-secondary text-l-semibold"
+        />
+        <Button
+          label={isSelected ? '삭제하기' : '추가하기'}
+          data-cy="add-schedule-button"
+          aria-label="add-schedule-button"
+          className="w-full h-[54px] flex justify-center items-center rounded-2xl bg-bg-tertiary text-[#F1F8F3] text-l-semibold"
+          onClick={() => {
+            !isSelected
+              ? handleAttendeeSchedules(dayOfWeek as DaysType, hhmm)
+              : handleRemoveAttendeeSchedules(dayOfWeek as DaysType, hhmm),
+              onClose()
+          }}
+        />
       </div>
     </BottomDrawer>
   )
