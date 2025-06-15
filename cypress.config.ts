878061bf7fa76c55ecd9aead536283659ca4256f
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-06-15 23:32:48 +0900

    fix: 'baseUrl'의 오타 수정 ('locahost' -> 'localhost')

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://locahost:5173',
+    baseUrl: 'http://localhost:5173',
   },
   experimentalWebKitSupport: true,
 })
