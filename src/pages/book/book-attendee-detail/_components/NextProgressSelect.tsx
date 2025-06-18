9a6bb1e17e0851947d14898e9913d8d8bccbe4f4
GanziMan <qjatn50089@gmail.com>
2025-06-19 03:18:45 +0900

    refactor: Improve NextProgressModal and NextProgressSelect components for better readability and maintainability

--- src/pages/book/book-attendee-detail/_components/NextProgressSelect.tsx
+++ src/pages/book/book-attendee-detail/_components/NextProgressSelect.tsx
@@ -1,50 +1,37 @@
-import * as Select from '@radix-ui/react-select'
-import {
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
-} from '@radix-ui/react-icons'
+import { CoursesResponse } from '@/api/AttendanceBookSchema'
 
-const options = [{ text: '(체르니50 다음 과정)', value: '1' }]
-
-const NextProgressSelect = () => {
+import DownArrowIcon from '@/assets/icons/ico-arrow-down.svg?react'
+import { NextProgressFormData } from '@/store/progressStore'
+export default function NextProgressSelect({
+  bookCourses,
+  nextGradeId,
+  updateFormData,
+}: {
+  bookCourses: CoursesResponse | undefined
+  nextGradeId: string
+  updateFormData: (key: keyof NextProgressFormData, value: string) => void
+}) {
   return (
-    <Select.Root onValueChange={() => {}}>
-      <Select.Trigger
-        className="inline-flex items-center justify-between w-full h-12 px-3 py-2 bg-white border  outline-none rounded-xl text-sm "
-        aria-label="관계"
+    <div className="relative">
+      <select
+        value={nextGradeId}
+        onChange={(e) => updateFormData('nextGradeId', e.target.value)}
+        className=" outline-none bg-white border border-[#E7E7E7] rounded-xl w-full h-12 px-4 text-m-medium appearance-none"
       >
-        <Select.Value placeholder="" />
-        <Select.Icon>
-          <ChevronDownIcon />
-        </Select.Icon>
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className="z-10 bg-white border border-gray-300 rounded-md shadow-md">
-          <Select.ScrollUpButton className="flex items-center justify-center h-6 bg-white">
-            <ChevronUpIcon />
-          </Select.ScrollUpButton>
-          <Select.Viewport className="p-2">
-            {options.map(({ text, value }) => (
-              <Select.Item
-                key={value}
-                value={value}
-                className="flex items-center p-2 rounded-md text-sm cursor-pointer hover:bg-gray-100"
-              >
-                <Select.ItemText>{text}</Select.ItemText>
-                <Select.ItemIndicator className="ml-auto text-green-500">
-                  <CheckIcon />
-                </Select.ItemIndicator>
-              </Select.Item>
-            ))}
-          </Select.Viewport>
-          <Select.ScrollDownButton className="flex items-center justify-center h-6 bg-white">
-            <ChevronDownIcon />
-          </Select.ScrollDownButton>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+        <option value="">과정을 선택하세요</option>
+
+        {(bookCourses?.courses.flatMap((course) => course.grades) || []).map(
+          (grade) => (
+            <option key={grade.id} value={grade.id}>
+              {grade.title}
+            </option>
+          ),
+        )}
+      </select>
+      <DownArrowIcon
+        className="pointer-events-none absolute inset-y-0 right-0 top-1/2 -translate-y-1/2"
+        color="#5d5d5d"
+      />
+    </div>
   )
 }
-
-export default NextProgressSelect
