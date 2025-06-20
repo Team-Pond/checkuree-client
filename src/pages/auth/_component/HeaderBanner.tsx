78083ba187ee0f6f8ad99d00ea61d93492fe49ba
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-06-20 20:23:32 +0900

    feat: Add SUIT font files and update HeaderBanner component for logo image

--- src/pages/auth/_component/HeaderBanner.tsx
+++ src/pages/auth/_component/HeaderBanner.tsx
@@ -2,14 +2,15 @@ export default function HeaderBanner() {
   return (
     <div className="flex flex-col gap-2">
       <img
-        src={"/images/logos/checkuree_logo.svg"}
+        src={'/images/logos/checkuree_logo.png'}
         width={284}
         height={48}
         alt="로고 이미지"
+        loading="lazy"
       />
       <p className="font-medium text-lg text-[#454545]">
         레슨 관리와 통계까지 손쉽게
       </p>
     </div>
-  );
+  )
 }
